b9875ccc503ac44a7494b861910829e059aeb945
Jens de Bruijn <j.a.debruijn@outlook.com>
2021-09-15 11:35:10 +0200

    some plottin gupdates

--- plot/plot.py
+++ plot/plot.py
@@ -2,13 +2,8 @@ import os
 import numpy as np
 import yaml
 
-with open('GEB.yml', 'r') as f:
-    config = yaml.load(f)
-
-def read_npy(name, dt, scenario=None):
+def read_npy(report_folder, name, dt, scenario=None):
     if scenario:
-        report_folder = os.path.join('report', scenario)
-    else:
-        report_folder = 'report'
+        report_folder = os.path.join(report_folder, scenario)
     fn = os.path.join(report_folder, name, dt.isoformat().replace(':', '').replace('-', '') + '.npy')
     return np.load(fn)
