4f68de4b31e25384f50970bce2c9776aaa58e220
Jens de Bruijn <j.a.debruijn@outlook.com>
2021-10-20 16:45:14 +0200

    fix file paths for Krishna basin

--- preprocessing/config.py
+++ preprocessing/config.py
@@ -1,6 +1,6 @@
 import os
 
-ROOT = 'DataDrive/GEB_Bhima'
+ROOT = 'DataDrive/GEB'
 INPUT = os.path.join(ROOT, 'input')
-INPUT_5MIN = os.path.join(ROOT, '..', 'GEB', 'input_5min')
-ORIGINAL_DATA = os.path.join(ROOT, '..', 'GEB', 'original_data')
+INPUT_5MIN = os.path.join(ROOT, 'input_5min')
+ORIGINAL_DATA = os.path.join(ROOT, 'original_data')
