81ac83881d283291f5544437c5bcf7315fd590a6
Jens de Bruijn <j.a.debruijn@outlook.com>
2022-02-15 15:11:57 +0100

    change model begin and end date

---
+++ run_server/custom_nogpu.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=custom
+#SBATCH --output=custom.out
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=3
+#SBATCH --mem=24G
+#SBATCH --time=200:00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=jens.de.bruijn@vu.nl
+
+source ~/.bashrc
+
+SCRIPT_DIR="/scistor/ivm/jbn271/Packages/GEB/"
+cd $SCRIPT_DIR
+
+conda activate abm  # activate conda environment
+
+python run.py --headless --scenario spinup
+python run.py --headless --scenario base
+python run.py --headless --scenario ngo_training
+python run.py --headless --scenario government_subsidies
+python run.py --headless --scenario base --switch_crops
+python run.py --headless --scenario ngo_training --switch_crops
+python run.py --headless --scenario government_subsidies --switch_crops
