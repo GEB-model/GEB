simplify plot and fix units

plot/new_plot.py
@@ -5,13 +5,21 @@ import datetime
 import pandas as pd
 import json
 import matplotlib.pyplot as plt
-from functools import cache
 import calendar
+import rasterio
 
-from plotconfig import config, ORIGINAL_DATA, INPUT
+from plotconfig import config, INPUT
 
-def is_sugarcane(x):
-    return np.count_nonzero(x == 4)
+SUM_DIVIDER = 1000
+
+def sum_sugarcane(x):
+    return np.count_nonzero(x == 4) / SUM_DIVIDER
+
+def sum_most(x):
+    return np.sum(x) / SUM_DIVIDER
+
+def irrigation_fn(x):
+    return np.mean(x) * SUM_DIVIDER  # m to mm
 
 class MyEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -27,7 +35,11 @@ class MyEncoder(json.JSONEncoder):
 class Plot:
     def __init__(self, scenario):
         self.report_folder = os.path.join(config['general']['report_folder'], scenario)
-        self.land_owners = np.load(os.path.join(self.report_folder, 'land_owners.npy'))
+        self.input_folder = os.path.join(config['general']['input_folder'])
+        with rasterio.open(os.path.join(self.input_folder, 'agents', 'farms.tif'), 'r') as src:
+            self.farms = src.read(1)
+        unique_land_owners = np.unique(self.farms)
+        self.n = unique_land_owners[unique_land_owners != -1].size
 
         self.cache_folder = os.path.join('plot', 'cache', scenario)
         os.makedirs(self.cache_folder, exist_ok=True)
@@ -40,14 +52,26 @@ class Plot:
         
         with rasterio.open(os.path.join(INPUT, 'areamaps', 'sub_cell_area.tif'), 'r') as src_cell_area:
             self.cell_area = src_cell_area.read(1)
+
+        vertical_index = np.arange(self.farms.shape[0]).repeat(self.farms.shape[1]).reshape(self.farms.shape)[self.farms != -1]
+        horizontal_index = np.tile(np.arange(self.farms.shape[1]), self.farms.shape[0]).reshape(self.farms.shape)[self.farms != -1]
+        self.pixels = np.zeros((self.n, 2), dtype=np.int32)
+        self.pixels[:,0] = np.round(np.bincount(self.farms[self.farms != -1], horizontal_index) / np.bincount(self.farms[self.farms != -1])).astype(int)
+        self.pixels[:,1] = np.round(np.bincount(self.farms[self.farms != -1], vertical_index) / np.bincount(self.farms[self.farms != -1])).astype(int)
         
         self.unmerged_HRU_indices = np.load(os.path.join(self.report_folder, 'unmerged_HRU_indices.npy'))
         self.scaling = np.load(os.path.join(self.report_folder, 'scaling.npy')).item()
+        
+        self.field_size = self.get_field_size()
+        
         self.reservoir_dependent_farmers = self.set_reservoir_dependent_farmers(2011, 2017)
         self.non_surface_water_dependent_farmers = self.set_non_surface_water_dependent_farmers(2011, 2017)
-        self.farmers_to_analyse = self.non_surface_water_dependent_farmers
+        # self.farmers_to_analyse = self.non_surface_water_dependent_farmers
+        self.farmers_to_analyse = ...
         self.command_areas = self.read_command_areas()
         self.activation_order = self.get_activation_order()
+
+        self.command_area_per_farmer = self.command_areas[self.pixels[:, 1], self.pixels[:, 0]]
         # plt.imshow(self.activation_order)
         # plt.show()
 
@@ -87,7 +111,6 @@ class Plot:
             # set infinite values to 1
             reservoir_irrigation_fraction[np.isinf(reservoir_irrigation_fraction)] = 1
             reservoir_dependent_farmers = reservoir_irrigation_fraction > .5
-            reservoir_dependent_farmers = self.farmer_array_to_fields(reservoir_dependent_farmers, -1, correct_for_field_size=False)
             np.save(cache_file, reservoir_dependent_farmers)
         else:
             reservoir_dependent_farmers = np.load(cache_file)
@@ -111,7 +134,6 @@ class Plot:
             surface_irrigation_total /= n_days
             # surface_irrigaton_dependent_farmers
             non_surface_water_dependent_farmers = surface_irrigation_total < 2
-            non_surface_water_dependent_farmers = self.farmer_array_to_fields(non_surface_water_dependent_farmers, -1, correct_for_field_size=False)
             
             np.save(cache_file, non_surface_water_dependent_farmers)
         else:
@@ -122,56 +144,30 @@ class Plot:
         fp = os.path.join(INPUT, 'routing', 'lakesreservoirs', 'subcommand_areas.tif')
         with rasterio.open(fp, 'r') as src:
             command_areas = src.read(1)
+        command_areas[self.submask == 1] = -1
         return command_areas
 
     def get_activation_order(self):
         if not os.path.exists(os.path.join(self.cache_folder, 'activation_order.npy')):
             activation_order = np.load(os.path.join(self.report_folder, 'activation_order.npy'))
-            activation_order = self.farmer_array_to_fields(activation_order, -1, correct_for_field_size=False)
             np.save(os.path.join(self.cache_folder, 'activation_order.npy'), activation_order)
         else:
             activation_order = np.load(os.path.join(self.cache_folder, 'activation_order.npy'))
         return activation_order
 
-    @cache
     def get_field_size(self):
         if not os.path.exists(os.path.join(self.cache_folder, 'field_size.npy')):
-            fields_decompressed = self.decompress_HRU(self.land_owners)
-            fields_decompressed = fields_decompressed[self.submask == 0]
-            is_field = np.where(fields_decompressed != -1)
-            cell_area = self.cell_area[self.submask == 0]
-            field_size = np.bincount(fields_decompressed[is_field], weights=cell_area[is_field])
+            is_field = np.where(self.farms != -1)
+            field_size = np.bincount(self.farms[is_field], weights=self.cell_area[is_field])
             np.save(os.path.join(self.cache_folder, 'field_size.npy'), field_size)
         else:
             field_size = np.load(os.path.join(self.cache_folder, 'field_size.npy'))
         return field_size
 
-    def farmer_array_to_fields(self, array, nofieldvalue, correct_for_field_size=True):
-        if correct_for_field_size:
-            field_size = self.get_field_size()
-            assert field_size.size == array.size
-            array /= field_size
-
-        array = np.take(array, self.land_owners)
-        array[self.land_owners == -1] = nofieldvalue
-        array = self.decompress_HRU(array)
-        return array
-
-    def decompress_HRU(self, array):
-        if np.issubdtype(array.dtype, np.integer):
-            nanvalue = -1
-        else:
-            nanvalue = np.nan
-        outarray = array[self.unmerged_HRU_indices]
-        outarray[self.submask] = nanvalue
-        return outarray
-
     def read_npy(self, name, dt):
+        dt -= datetime.timedelta(days=1)
         fn = os.path.join(self.report_folder, name, dt.isoformat().replace(':', '').replace('-', '') + '.npy')
         return np.load(fn)
-
-    def get_command_area_indices(self, command_area_id, subset=...):
-        return np.where(self.command_areas[subset] == command_area_id)
 
     def get_honeybees_data(self, varname, start_year, end_year, fileformat='csv'):
         df = pd.read_csv(os.path.join(self.report_folder, varname + '.' + fileformat), index_col=0)
@@ -190,6 +186,7 @@ class Plot:
             day = datetime.date(year, 1, 1)
             # loop through all days in year
             total = None
+            n = 0
             while day < datetime.date(year + 1, 1, 1):
                 # read the data
                 array = self.read_npy(name, day)
@@ -199,6 +196,8 @@ class Plot:
                     total += array
                 # go to next day
                 day += datetime.timedelta(days=1)
+                n += 1
+            total /= n
         elif mode == 'first_day_of_year':
             day = datetime.date(year, 1, 1)
             total = self.read_npy(name, day)
@@ -212,6 +211,8 @@ class Plot:
             print('year', year, 'for', name, 'not in cache')
 
             total = self.read_arrays(year, name, mode=mode)
+            if correct_for_field_size:
+                total /= self.field_size
 
             mapping = np.full(self.command_areas.max() + 1, 0, dtype=np.int32)
             command_area_ids = np.unique(self.command_areas)
@@ -220,25 +221,21 @@ class Plot:
             mapping[command_area_ids] = np.arange(0, command_area_ids.size, dtype=np.int32)
             command_areas_mapped = mapping[self.command_areas]
             command_areas_mapped[self.command_areas == -1] = -1
+
+            assert total.size == self.n
             
-            array = self.farmer_array_to_fields(total, 0, correct_for_field_size=correct_for_field_size)
-            array = array[self.farmers_to_analyse]
-            # plt.imshow(array)
-            # plt.show()
+            array = total[self.farmers_to_analyse]
 
             by_command_area = {}
             for command_area_id in command_area_ids:
                 command_area_id = command_area_id.item()
-                command_area = self.get_command_area_indices(command_area_id, subset=self.farmers_to_analyse)
-                activation_order_area = self.activation_order[self.farmers_to_analyse][command_area]
-                if (activation_order_area == -1).all():
-                    continue
-                activation_order_area_filtered = activation_order_area[activation_order_area != -1]
-                array_area = array[command_area][activation_order_area != -1]
-                activation_order_median = np.percentile(activation_order_area_filtered, 50)
+                farmers_in_command_area = self.command_area_per_farmer[self.farmers_to_analyse] == command_area_id
+                activation_order_area = self.activation_order[self.farmers_to_analyse][farmers_in_command_area]
+                activation_order_median = np.percentile(activation_order_area, 50)
+                array_command_area = array[self.farmers_to_analyse][farmers_in_command_area]
                 
-                head_end = fn(array_area[activation_order_area_filtered < activation_order_median])
-                tail_end = fn(array_area[activation_order_area_filtered >= activation_order_median])
+                head_end = fn(array_command_area[activation_order_area < activation_order_median])
+                tail_end = fn(array_command_area[activation_order_area >= activation_order_median])
                 by_command_area[command_area_id] = {
                     'head_end': head_end,
                     'tail_end': tail_end,
@@ -257,9 +254,9 @@ class Plot:
             print('year', year, 'for', name, 'not in cache')
 
             total = self.read_arrays(year, name, mode=mode)
-            field_size = self.get_field_size()
             if correct_for_field_size:
-                total = total / field_size
+                total /= self.field_size
+            field_size = self.get_field_size()
             # get median field size
             median_field_size = np.percentile(field_size, 50)
             small_fields = field_size < median_field_size
@@ -275,7 +272,20 @@ class Plot:
                 by_field_size = json.load(f)
         return by_field_size
 
-    def plot_tail_vs_head_end(self, start_year, end_year, ax, label, *args, **kwargs):
+    def format_ax(self, ax, ylabel, ymax=None):
+        ax.legend()
+        # rotate all y tick labels and center them
+        for tick in ax.get_yticklabels():
+            tick.set_rotation(90)
+            tick.set_verticalalignment('center')
+        ax.set_xlabel('year')
+        ax.set_ylabel(ylabel)
+        if ymax is not None:
+            ax.set_ylim(0, ymax)
+        elif ylabel.startswith('farmers with'):
+            ax.set_ylim(bottom=0, top=self.n / SUM_DIVIDER * 1.05)
+
+    def plot_tail_vs_head_end(self, start_year, end_year, ax, *args, **kwargs):
         tail_end = []
         head_end = []
         years = []
@@ -283,7 +293,7 @@ class Plot:
             by_command_area = self.get_values_head_vs_tail(*args, year=year, **kwargs)
             year_values_tail_end = 0
             year_values_head_end = 0
-            for command_area_id, values in by_command_area.items():
+            for values in by_command_area.values():
                 year_values_tail_end += values['tail_end']
                 year_values_head_end += values['head_end']
             tail_end.append(year_values_tail_end)
@@ -291,11 +301,8 @@ class Plot:
             years.append(year)
         ax.plot(years, tail_end, label='tail end')
         ax.plot(years, head_end, label='head end')
-        ax.legend()
-        ax.set_xlabel('year')
-        ax.set_ylabel(label)
 
-    def plot_small_vs_large_farmer(self, start_year, end_year, ax, label, *args, **kwargs):
+    def plot_small_vs_large_farmer(self, start_year, end_year, ax, *args, **kwargs):
         small_farmer = []
         large_farmer = []
         years = []
@@ -307,32 +314,33 @@ class Plot:
 
         ax.plot(years, small_farmer, label='small farmer', color='red')
         ax.plot(years, large_farmer, label='large farmer', color='green')
-        ax.legend()
-        ax.set_xlabel('year')
-        ax.set_ylabel(label)
-
 
     def create_plot(self, start_year, end_year):
         hydraulic_head, hydraulic_head_years = self.get_honeybees_data('hydraulic head', start_year=start_year, end_year=end_year)
         
-        fig, ax = plt.subplots(3, 4, figsize=(20, 20))
+        fig, ax = plt.subplots(2, 3, figsize=(20, 8))
 
-        ax[0][0].plot(hydraulic_head, hydraulic_head_years, label='hydraulic head')
+        self.plot_tail_vs_head_end(start_year, end_year, ax[0][0], name="reservoir irrigation", fn=irrigation_fn, correct_for_field_size=True, mode='full_year')
+        self.format_ax(ax[0][0], ylabel="reservoir irrigation (mm/day)", ymax=6)
+        self.plot_tail_vs_head_end(start_year, end_year, ax[0][1], name="groundwater irrigation", fn=irrigation_fn, correct_for_field_size=True, mode='full_year')
+        self.format_ax(ax[0][1], ylabel="groundwater irrigation (mm/day)", ymax=6)
+        self.plot_tail_vs_head_end(start_year, end_year, ax[0][2], name="channel irrigation", fn=irrigation_fn, correct_for_field_size=True, mode='full_year')
+        self.format_ax(ax[0][2], ylabel="channel irrigation (mm/day)", ymax=6)
         
-        self.plot_tail_vs_head_end(start_year, end_year, ax[1][0], label="reservoir irrigation", name="reservoir irrigation", fn=np.sum, correct_for_field_size=True, mode='full_year')
-        self.plot_tail_vs_head_end(start_year, end_year, ax[1][1], label="groundwater irrigation", name="groundwater irrigation", fn=np.sum, correct_for_field_size=True, mode='full_year')
-        self.plot_tail_vs_head_end(start_year, end_year, ax[2][0], label="channel irrigation", name="channel irrigation", fn=np.sum, correct_for_field_size=True, mode='full_year')
-        self.plot_tail_vs_head_end(start_year, end_year, ax[1][2], label="farmers with sugarcane", name="crops_kharif", fn=is_sugarcane, correct_for_field_size=False, mode='first_day_of_year')
-        self.plot_tail_vs_head_end(start_year, end_year, ax[1][3], label="farmers with well", name="well_irrigated", fn=np.sum, correct_for_field_size=False, mode='first_day_of_year')
-        
-        self.plot_small_vs_large_farmer(start_year, end_year, ax[2][2], label="farmers with sugarcane", name="crops_kharif", fn=is_sugarcane, correct_for_field_size=False, mode='first_day_of_year')
-        self.plot_small_vs_large_farmer(start_year, end_year, ax[2][3], label="farmers with well", name="well_irrigated", fn=np.sum, correct_for_field_size=False, mode='first_day_of_year')
+        self.plot_tail_vs_head_end(start_year, end_year, ax[1][0], name="crops_kharif", fn=sum_sugarcane, correct_for_field_size=False, mode='first_day_of_year')
+        self.plot_small_vs_large_farmer(start_year, end_year, ax[1][0], name="crops_kharif", fn=sum_sugarcane, correct_for_field_size=False, mode='first_day_of_year')
+        self.format_ax(ax[1][0], ylabel=f"farmers with sugarcane (×{SUM_DIVIDER})")
+        self.plot_tail_vs_head_end(start_year, end_year, ax[1][1], name="well_irrigated", fn=sum_most, correct_for_field_size=False, mode='first_day_of_year')
+        self.plot_small_vs_large_farmer(start_year, end_year, ax[1][1], name="well_irrigated", fn=sum_most, correct_for_field_size=False, mode='first_day_of_year')
+        self.format_ax(ax[1][1], ylabel=f"farmers with well (×{SUM_DIVIDER})")
 
-        plt.legend()
+        ax[1][2].plot(hydraulic_head_years, hydraulic_head, label="hydraulic head")
+        self.format_ax(ax[1][2], ylabel="mean hydraulic head")
+
         # plt.show()
         plt.savefig('plot.png')
 
-p = Plot('sugarcane')
+p = Plot('sprinkler')
 
 START_YEAR = 2011
 END_YEAR = 2017