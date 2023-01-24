8e96eb07b1a924c2963537a92c5091e02a98133f
Jens de Bruijn <j.a.debruijn@outlook.com>
2023-01-24 10:44:12 +0100

    run all together

---
+++ run_server/all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=all
+#SBATCH --output=all.out
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks=2
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=20G
+#SBATCH --time=100:00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=jens.de.bruijn@vu.nl
+
+source ~/.bashrc
+
+SCRIPT_DIR="$HOME/GEB/GEB_private/"
+cd $SCRIPT_DIR
+
+module load cuda10.2/toolkit/10.2.89  # load cuda environment
+conda activate GEB  # activate conda environment
+
+python run.py --scenario spinup --config krishna.yml --GPU
+srun --exclusive --ntasks=1 --nodes=1 --cpus-per-task=$SLURM_CPUS_PER_TASK python run.py --scenario sprinkler --config krishna.yml --GPU &
+srun --exclusive --ntasks=1 --nodes=1 --cpus-per-task=$SLURM_CPUS_PER_TASK python run.py --scenario sugarcane --config krishna.yml --GPU &
+wait
+python run.py --scenario base --config krishna.yml --GPU
