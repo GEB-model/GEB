fe12dac7996ef7b6824474a219c44bc73301c485
Jens de Bruijn <j.a.debruijn@outlook.com>
2023-07-03 17:25:28 +0200

    include logger

--- build_model.py
+++ build_model.py
@@ -1,16 +1,48 @@
+import logging
 from hydromt_geb import GEBModel
 from hydromt.config import configread
 
+from config import INPUT, config
+
 if __name__ == '__main__':
     yml = r"./hydromt.yml"
-    root = r"../DataDrive/sandbox/input"
 
     data_libs = [r"../DataDrive/GEB/original_data/data_catalog.yml"]
     opt = configread(yml)
+    logger = logging.getLogger(__name__)
+    # set log level to debug
+    logger.setLevel(logging.DEBUG)
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    # create formatter
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    # add formatter to ch
+    ch.setFormatter(formatter)
+    # add ch to logger
+    logger.addHandler(ch)
+    # add file handler
+    fh = logging.FileHandler(INPUT / 'hydromt.log')
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
     
-    geb_model = GEBModel(root=root, mode='w+', data_libs=data_libs)
-    # Bhima
-    # geb_model.build(opt=opt, region={'subbasin': [[75.895273], [17.370473]], 'bounds': [66.55, 4.3, 93.17, 35.28]})
+    geb_model = GEBModel(
+        root=INPUT,
+        mode='w+',
+        data_libs=data_libs,
+        logger=logger,
+    )
 
-    # Sanctuary
-    geb_model.build(opt=opt, region={'subbasin': [[73.98727], [19.00465]], 'bounds': [66.55, 4.3, 93.17, 35.28]})
+    poor_point = config['general']['poor_point']
+    geb_model.build(
+        opt=opt,
+        region={
+            'subbasin': [
+                [poor_point[0]], [poor_point[1]]
+            ],
+            'bounds': [66.55, 4.3, 93.17, 35.28]  # TODO: remove need to specify bounds
+        },
+    )
