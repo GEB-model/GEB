5f750d82c4de0bd508a98b8ca00d23072ac25151
Jens de Bruijn <j.a.debruijn@outlook.com>
2023-08-28 14:24:23 +0200

    init

--- run_server/build_model.sh
+++ run_server/build_model.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=base
-#SBATCH --output=base.out
+#SBATCH --job-name=build_model
+#SBATCH --output=build_model.out
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=20G
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=100G
 #SBATCH --time=100:00:00
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=jens.de.bruijn@vu.nl
@@ -17,4 +17,9 @@ cd $SCRIPT_DIR
 
 conda activate GEB  # activate conda environment
 
-python build_model.py --config $1
+python build_model.py --config $1 && \
+python preprocessing/krishna/2_create_tehsil_data_table.py --config $1 && \
+python "preprocessing/krishna/3_parse_IHDS I.py" --config $1 && \
+python preprocessing/krishna/4_iterative_proportional_fitting.py --config $1 && \
+python preprocessing/krishna/5_create_farmers.py --config $1 && \
+python build_model_farmers.py --config $1
