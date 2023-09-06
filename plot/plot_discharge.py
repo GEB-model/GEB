ac620d2cd8ceae4005bbb68fe5e688c1b27dc2c2
Jens de Bruijn <j.a.debruijn@outlook.com>
2023-09-06 10:01:50 +0200

    move custom plotting and preprocessing outside the package

--- plot/plot_discharge.py
+++
@@ -1,7 +0,0 @@
-from hydro_stats import get_discharge
-import matplotlib.pyplot as plt
-
-dates, values = get_discharge('spinup')
-
-plt.plot(dates, values)
-plt.show()
--- preprocessing/krishna/preconfig.py
+++
@@ -1,8 +0,0 @@
-import sys
-from pathlib import Path
-
-sys.path.insert(0, str(Path(__file__).absolute().parent.parent.parent))
-from config import *
-
-ORIGINAL_DATA = Path(config['general']['original_data'])
-PREPROCESSING_FOLDER = Path(config['general']['preprocessing_folder'])
