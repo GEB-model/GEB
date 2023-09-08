23bbc86aa72a63cf5667695d8bc8ee489db29a90
Jens de Bruijn <j.a.debruijn@outlook.com>
2023-09-08 13:53:26 +0200

    add agent array, and test framework

--- geb/agents/__init__.py
+++ geb/agents/__init__.py
@@ -4,6 +4,141 @@ from .ngo import NGO
 from .government import Government
 from .reservoir_operators import ReservoirOperators
 
+import numpy as np
+
+class AgentArray(np.ndarray):
+    implemented_methods = (
+        'mean',
+        'sum',
+        'std',
+        'min',
+        'max',
+        'n',
+        '_n',
+        'ndim',
+        'shape',
+        'dtype',
+        'max_size',
+        'size',
+        '__class__',
+        'view',
+        '__repr__',
+        '__str__',
+    )
+    def __new__(cls, input_array, n=None, max_size=None):
+        if n is None and max_size is None:
+            raise ValueError("Either n or max_size must be given")
+        elif n is not None and max_size is not None:
+            raise ValueError("Only one of n or max_size can be given")
+        
+        if max_size:
+            obj = np.empty_like(input_array, shape=max_size).view(cls)
+            n = input_array.size
+            obj[:n] = input_array
+            obj._n = n
+        elif n:
+            obj = input_array.view(cls)
+            obj._n = n
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is None:
+            return
+        self._n = getattr(obj, 'n', None)
+
+    def __array_function__(self, func, types, args, kwargs):
+        args_ = []
+        for arg in args:
+            if isinstance(arg, AgentArray):
+                args_.append(arg[:arg.n].view(np.ndarray))
+            else:
+                args_.append(arg)
+        return func(*args_, **kwargs)
+
+    def __getattribute__(self, name):
+        if name in AgentArray.implemented_methods:
+            return super().__getattribute__(name)
+        else:
+            raise NotImplementedError("Attribute {} not implemented".format(name))
+    
+    def __array_ufunc__(self, ufunc, method, *inputs, out=None, **kwargs):
+        args = []
+        in_no = []
+        for i, input_ in enumerate(inputs):
+            if isinstance(input_, AgentArray):
+                in_no.append(i)
+                # print('i', input_, input_.n)
+                args.append(input_[:input_.n].view(np.ndarray))
+            else:
+                args.append(input_)
+
+        if method == 'reduce':
+            return super().__array_ufunc__(ufunc, method, *args, **kwargs)
+
+        outputs = out
+        out_no = []
+        if outputs:
+            out_args = []
+            for j, output in enumerate(outputs):
+                if isinstance(output, AgentArray):
+                    out_no.append(j)
+                    out_args.append(output[:output.n].view(np.ndarray))
+                else:
+                    out_args.append(output)
+            kwargs['out'] = tuple(out_args)
+        else:
+            outputs = (None,) * ufunc.nout
+
+        results = super().__array_ufunc__(ufunc, method, *args, **kwargs)
+        if results is NotImplemented:
+            return NotImplemented
+
+        if method == 'at':
+            if isinstance(inputs[0], AgentArray):
+                inputs[0].n = n
+            return
+
+        if ufunc.nout == 1:
+            results = (results,)
+
+        results = tuple(
+            ((np.asarray(result) if method == 'reduce' else np.asarray(result).view(AgentArray))
+            if output is None else output)
+            for result, output in zip(results, outputs)
+        )
+        for result in results:
+            if isinstance(result, AgentArray):
+                result.n = self._n
+        return results[0] if len(results) == 1 else results
+
+    @property
+    def shape(self):
+        return self[:self._n].view(np.ndarray).shape
+
+    @property
+    def size(self):
+        return self[:self._n].view(np.ndarray).size
+
+    @property
+    def n(self):
+        return self._n
+
+    @n.setter
+    def n(self, value):
+        if value > self.max_size:
+            raise ValueError("n cannot exceed max_size")
+        self._n = value
+
+    @property
+    def max_size(self):
+        return self.view(np.ndarray).size
+
+    def __setitem__(self, key, value):
+        self[:self._n].view(np.ndarray).__setitem__(key, value)
+
+    def __getitem__(self, key):
+        return self.view(np.ndarray)[:self._n].__getitem__(key)
+
 class Agents:
     """This class initalizes all agent classes, and is used to activate the agents each timestep.
 
@@ -14,12 +149,12 @@ class Agents:
         self.model = model
         self.farmers = Farmers(model, self, 0.1)
         self.reservoir_operators = ReservoirOperators(model, self)
-        self.ngo = NGO(model, self)
+        self._ngo = NGO(model, self)
         self.government = Government(model, self)
 
     def step(self) -> None:
         """This function is called every timestep and activates the agents in order of NGO, government and then farmers."""
-        self.ngo.step()
+        self._ngo.step()
         self.government.step()
         self.farmers.step()
         self.reservoir_operators.step()
---
+++ tests/test_agent_array.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+from geb.agents import AgentArray
+    
+def test_agent_array():
+    # Test initialization with max_size
+    a = AgentArray(np.array([1, 2, 3]), max_size=10)
+    assert np.array_equal(a, np.array([1, 2, 3]))
+    assert a.max_size == 10
+    assert a.n == 3
+
+    # Test addition with scalar
+    a += 1
+    assert np.array_equal(a, np.array([2, 3, 4]))
+
+    # Test addition with array
+    b = np.array([1, 2, 3])
+    a += b
+    assert np.array_equal(a, np.array([3, 5, 7]))
+
+    # Test item assignment
+    a[0] = 4
+    assert np.array_equal(a, np.array([4, 5, 7]))
+
+    # Test slicing
+    assert np.array_equal(a[:2], np.array([4, 5]))
+
+    # Test array methods
+    assert np.array_equal(np.unique(a, return_counts=True), (np.array([4, 5, 7]), np.array([1, 1, 1])))
+    assert a.sum() == 16
+    assert np.array_equal(a.mean(), 16/3)
+    assert a.std() == np.std(a)
+    assert a.min() == np.min(a)
+    assert a.max() == np.max(a)
+
+    # Test setting n
+    a.n = 2
+    assert np.array_equal(a, np.array([4, 5]))
+    assert a.n == 2
+
+    # Test setting n and adding new items
+    a.n = 3
+    a[2] = 6
+    assert np.array_equal(a, np.array([4, 5, 6]))
+    assert a.n == 3
+
+    # Test setting n and adding new items beyond max_size
+    a.n = 4
+    a[3] = 7
+    assert np.array_equal(a, np.array([4, 5, 6, 7]))
+    assert a.n == 4
+    assert a.max_size == 10
+    
+    # Test setting n to exceed max_size
+    try:
+        a.n = 11
+    except ValueError:
+        pass
+    else:
+        assert False, "Expected ValueError"
+
+    # Test size property
+    assert a.size == 4
+
+if __name__ == '__main__':
+    test_agent_array()
