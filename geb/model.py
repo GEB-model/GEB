99cae950221e3bba11736451877e4a93b8b7e984
Jens de Bruijn <j.a.debruijn@outlook.com>
2024-01-18 11:12:03 +0100

    flush print

--- geb/model.py
+++ geb/model.py
@@ -242,7 +242,7 @@ class GEBModel(ABM_Model, CWatM_Model):
         else:
             n = step_size
         for _ in range(n):
-            print(self.current_time)
+            print(self.current_time, flush=True)
             t0 = time()
             self.data.step()
             ABM_Model.step(self, 1, report=False)
