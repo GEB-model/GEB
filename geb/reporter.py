74f0c5987f0f32f14f32c08f1e2e4a3a71d191ca
Jens de Bruijn <j.a.debruijn@outlook.com>
2024-02-29 16:34:16 +0100

    add retry mechanism for writing to netcdfs which may help on networked drives

--- geb/reporter.py
+++ geb/reporter.py
@@ -22,6 +22,7 @@ except ImportError:
 from operator import attrgetter
 
 from honeybees.reporter import Reporter as ABMReporter
+import time
 
 
 class CWatMReporter(ABMReporter):
@@ -227,22 +228,33 @@ class CWatMReporter(ABMReporter):
             if np.isin(
                 np.datetime64(self.model.current_time), self.variables[name].time
             ):
-                with netCDF4.Dataset(
-                    self.model.config["report_cwatm"][name]["absolute_path"], "a"
-                ) as nc:
-                    var = nc.variables[name]
-                    time_index = self.variables[name].time == np.datetime64(
-                        self.model.current_time
-                    )
-                    if "substeps" in conf:
-                        time_index_start = np.where(time_index)[0][0]
-                        time_index_end = time_index_start + conf["substeps"]
-                        var[time_index_start:time_index_end, ...] = value
-                    else:
-                        var[time_index, ...] = (
-                            value  # Assuming new_data is the new values for that time slice
-                        )
-                    nc.sync()
+                max_retries = 10
+                retry_delay = 1
+
+                for retry in range(max_retries):
+                    try:
+                        with netCDF4.Dataset(
+                            self.model.config["report_cwatm"][name]["absolute_path"],
+                            "a",
+                        ) as nc:
+                            var = nc.variables[name]
+                            time_index = self.variables[name].time == np.datetime64(
+                                self.model.current_time
+                            )
+                            if "substeps" in conf:
+                                time_index_start = np.where(time_index)[0][0]
+                                time_index_end = time_index_start + conf["substeps"]
+                                var[time_index_start:time_index_end, ...] = value
+                            else:
+                                var[time_index, ...] = (
+                                    value  # Assuming new_data is the new values for that time slice
+                                )
+                            nc.sync()
+                        break
+                    except FileNotFoundError:
+                        if retry == max_retries - 1:
+                            raise
+                        time.sleep(retry_delay)
         else:
             raise ValueError(f"{conf['format']} not recognized")
 
