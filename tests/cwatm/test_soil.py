d45921c3efc14acd83e0e541eb84bb8b1a5ac88e
Jens de Bruijn <j.a.debruijn@outlook.com>
2024-08-05 16:30:37 +0200

    new tests

--- tests/cwatm/test_soil.py
+++ tests/cwatm/test_soil.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from .modules.soil import (
+from cwatm.modules.soil import (
     get_critical_soil_moisture_content,
     get_fraction_easily_available_soil_water,
     get_transpiration_reduction_factor,
