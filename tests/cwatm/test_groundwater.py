77f2fd1041fa2b848541ba2515a2f3136714e701
Jens de Bruijn <j.a.debruijn@outlook.com>
2024-08-07 14:20:21 +0200

    tests for irrigular modflow grid

--- tests/cwatm/test_groundwater.py
+++ tests/cwatm/test_groundwater.py
@@ -11,37 +11,52 @@ class DummyModel:
         self.simulation_root = tmp_folder / "modflow"
 
 
+XSIZE = 12
+YSIZE = 10
+NLAY = 1
+
 # Create a topography 2D map
-x = np.linspace(-5, 5, 10)
-y = np.linspace(-5, 5, 10)
+x = np.linspace(-5, 5, XSIZE)
+y = np.linspace(-5, 5, YSIZE)
 x, y = np.meshgrid(x, y)
 
 topography = np.exp2(-(x**2) - y**2 + 5)
-basin_mask = np.zeros((10, 10), dtype=bool)
-basin_mask[-1] = True
+basin_mask = np.zeros((YSIZE, XSIZE), dtype=bool)
+basin_mask[0] = True
 basin_mask[-3:-1, 0:3] = True
 
-XSIZE = 10
-YSIZE = 10
-NLAY = 1
+# Create a topography 2D map
+x_vertices, y_vertices = np.meshgrid(
+    np.linspace(0, XSIZE * 10, XSIZE + 1), np.linspace(0, YSIZE * 10, YSIZE + 1)
+)
+
+
+def compress(array, mask):
+    return array[..., ~mask]
+
+
+def decompress(array, mask):
+    out = np.full(mask.shape, np.nan)
+    out[~mask] = array
+    return out
+
 
 default_params = {
     "model": DummyModel(),
     "name": "test_model",
     "ndays": 20,
-    "specific_storage": np.full((NLAY, YSIZE, XSIZE), 0),
-    "specific_yield": np.full((NLAY, YSIZE, XSIZE), 0.4),
-    "nlay": NLAY,
-    "nrow": 10,
-    "ncol": 10,
-    "row_resolution": 10,
-    "col_resolution": 10,
-    "topography": topography,
-    "bottom_soil": topography - 2,
-    "bottom": topography - np.full((NLAY, YSIZE, XSIZE), 10),
+    "specific_storage": compress(np.full((NLAY, YSIZE, XSIZE), 0), basin_mask),
+    "specific_yield": compress(np.full((NLAY, YSIZE, XSIZE), 0.4), basin_mask),
+    "nrow": YSIZE,
+    "ncol": XSIZE,
+    "x_coordinates_vertices": x_vertices,
+    "y_coordinates_vertices": y_vertices,
+    "topography": compress(topography, basin_mask),
+    "bottom_soil": compress(topography - 2, basin_mask),
+    "bottom": compress(topography - np.full((NLAY, YSIZE, XSIZE), 10), basin_mask),
     "basin_mask": basin_mask,
-    "head": topography - 2,
-    "hydraulic_conductivity": np.full((NLAY, YSIZE, XSIZE), 1),
+    "head": compress(topography - 2, basin_mask),
+    "hydraulic_conductivity": compress(np.full((NLAY, YSIZE, XSIZE), 1), basin_mask),
     "verbose": True,
 }
 
@@ -49,32 +64,34 @@ default_params = {
 def test_modflow_simulation_initialization():
     sim = ModFlowSimulation(**default_params)
     assert sim.name == "TEST_MODEL"
-    assert sim.nrow == 10
-    assert sim.ncol == 10
-    assert sim.n_active_cells == 84
+    assert sim.nrow == YSIZE
+    assert sim.ncol == XSIZE
+    assert sim.n_active_cells == (~basin_mask).sum()
+    assert (sim.area == 100.0).all()
 
 
 def test_recharge():
     parameters = default_params.copy()
-    parameters["head"] = topography - 5  # set head lower than drainage
+    parameters["head"] = compress(
+        topography - 5, basin_mask
+    )  # set head lower than drainage
 
     sim = ModFlowSimulation(**parameters)
 
     groundwater_content_prev = np.nansum(sim.groundwater_content_m3)
 
-    sim.set_recharge_m(np.full((10, 10), 0.01))
+    recharge = np.full((YSIZE, XSIZE), 0.01)
+    sim.set_recharge_m(compress(recharge, sim.basin_mask))
     sim.step()
 
-    drainage = np.nansum(sim.drainage_m3)
-    assert np.nansum(drainage) == 0
+    drainage_m3 = np.nansum(sim.drainage_m3)
+    assert np.nansum(drainage_m3) == 0
     groundwater_content = np.nansum(sim.groundwater_content_m3)
-    recharge = np.nansum(sim.recharge_m3)
 
-    assert np.nansum(
-        sim.recharge_m * sim.row_resolution * sim.col_resolution
-    ) == np.nansum(recharge)
+    assert np.nansum(sim.recharge_m) == np.nansum(sim.recharge_m3 / sim.area)
 
-    balance_pre = groundwater_content_prev + recharge - drainage
+    recharge_m3 = np.nansum(sim.recharge_m3)
+    balance_pre = groundwater_content_prev + recharge_m3 - drainage_m3
     balance_post = groundwater_content
 
     assert math.isclose(balance_pre, balance_post, rel_tol=1e-5)
@@ -84,10 +101,13 @@ def test_recharge():
 
 def test_drainage():
     parameters = default_params.copy()
-    parameters["topography"] = np.full((10, 10), 0)
-    parameters["bottom_soil"] = parameters["topography"] - 2
-    parameters["bottom"] = parameters["topography"] - np.full((NLAY, YSIZE, XSIZE), 10)
-    parameters["head"] = parameters["topography"]
+    topography = np.full((YSIZE, XSIZE), 0)
+    parameters["topography"] = compress(topography, basin_mask)
+    parameters["bottom_soil"] = compress(topography - 2, basin_mask)
+    parameters["bottom"] = compress(
+        topography - np.full((NLAY, YSIZE, XSIZE), 10), basin_mask
+    )
+    parameters["head"] = compress(topography, basin_mask)
 
     sim = ModFlowSimulation(**parameters)
 
@@ -98,9 +118,7 @@ def test_drainage():
     drainage = np.nansum(sim.drainage_m3)
     assert drainage.sum() > 0
 
-    assert np.nansum(
-        sim.drainage_m * sim.row_resolution * sim.col_resolution
-    ) == np.nansum(drainage)
+    assert np.nansum(sim.drainage_m * sim.area) == np.nansum(drainage)
 
     groundwater_content = np.nansum(sim.groundwater_content_m3)
 
@@ -125,25 +143,23 @@ def test_drainage():
 
 def test_wells():
     parameters = default_params.copy()
-    parameters["head"] = topography - 5  # set head lower than drainage
+    parameters["head"] = compress(
+        topography - 5, basin_mask
+    )  # set head lower than drainage
 
     sim = ModFlowSimulation(**parameters)
 
     groundwater_content_prev = np.nansum(sim.groundwater_content_m3)
 
-    groundwater_abstracton = np.full((10, 10), 0.10)
+    groundwater_abstracton = np.full((YSIZE, XSIZE), 0.10)
     groundwater_abstracton[0, 0] = 0.10
     groundwater_abstracton[1, 1] = 0.05
     groundwater_abstracton[4, 5] = 0.20
     groundwater_abstracton[parameters["basin_mask"]] = np.nan
 
-    total_abstraction = (
-        np.nansum(groundwater_abstracton)
-        * default_params["row_resolution"]
-        * default_params["col_resolution"]
-    )
+    total_abstraction = np.nansum(groundwater_abstracton[~basin_mask] * sim.area)
 
-    sim.set_groundwater_abstraction_m(groundwater_abstracton)
+    sim.set_groundwater_abstraction_m(compress(groundwater_abstracton, sim.basin_mask))
     sim.step()
 
     drainage = np.nansum(sim.drainage_m3)
@@ -163,22 +179,28 @@ def visualize_modflow_results(sim, axes):
     (ax1, ax2, ax3, ax4) = axes
 
     # Plot topography
-    im1 = ax1.imshow(sim.topography, cmap="terrain")
+    im1 = ax1.imshow(
+        decompress(
+            sim.topography,
+            sim.basin_mask,
+        ),
+        cmap="terrain",
+    )
     ax1.set_title("Topography")
     plt.colorbar(im1, ax=ax1, label="Elevation (m)")
 
     # Plot groundwater head
-    im2 = ax2.imshow(sim.decompress(sim.head), cmap="viridis")
+    im2 = ax2.imshow(decompress(sim.head, sim.basin_mask), cmap="viridis")
     ax2.set_title("Groundwater Head")
     plt.colorbar(im2, ax=ax2, label="Head (m)")
 
     # Plot groundwater depth
-    im3 = ax3.imshow(sim.groundwater_depth, cmap="RdYlBu")
+    im3 = ax3.imshow(decompress(sim.groundwater_depth, sim.basin_mask), cmap="RdYlBu")
     ax3.set_title("Groundwater Depth")
     plt.colorbar(im3, ax=ax3, label="Depth (m)")
 
     # Plot drainage
-    im4 = ax4.imshow(sim.drainage_m, cmap="Blues")
+    im4 = ax4.imshow(decompress(sim.drainage_m, sim.basin_mask), cmap="Blues")
     ax4.set_title("Drainage")
     plt.colorbar(im4, ax=ax4, label="Drainage (m/day)")
 
@@ -191,13 +213,16 @@ def test_modflow_simulation_with_visualization():
 
     # Run the simulation for a few steps
     for i in range(5):
-        sim.set_recharge_m(np.random.uniform(0, 0.001, size=(10, 10)))
+        recharge = np.random.uniform(0, 0.001, size=(YSIZE, XSIZE))
+        sim.set_recharge_m(compress(recharge, sim.basin_mask))
 
-        groundwater_abstracton = np.full((10, 10), 0.0)
-        groundwater_abstracton[1, 1] = 0.05
+        groundwater_abstracton = np.full((YSIZE, XSIZE), 0.0)
+        groundwater_abstracton[2, 2] = 1.25
         groundwater_abstracton[3, 5] = 0.20
 
-        sim.set_groundwater_abstraction_m(groundwater_abstracton)
+        sim.set_groundwater_abstraction_m(
+            compress(groundwater_abstracton, sim.basin_mask)
+        )
         sim.step()
 
         # Visualize the results
