abd3dd3b76f38835cfe81da33094a3a4d68fb817
Jens de Bruijn <j.a.debruijn@outlook.com>
2024-08-23 16:38:08 +0200

    bump version number

--- geb/__init__.py
+++ geb/__init__.py
@@ -1,3 +1,3 @@
 """GEB aims to simulate both environment, for now the hydrological system, the individual behaviour of people and their interactions at large scale."""
 
-__version__ = "1.0.0dev"
+__version__ = "1.0.0a1"
