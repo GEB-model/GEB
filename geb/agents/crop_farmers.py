fixes calculation of yield ratio, make some more variables human readable

geb/agents/crop_farmers.py
@@ -914,13 +914,13 @@ class CropFarmers(AgentBaseClass):
             "expected_utility"
         ]["adaptation_sprinkler"]["yield_multiplier"]
 
-        self.var.actual_transpiration_crop = self.var.load_initial(
-            "actual_transpiration_crop",
+        self.var.actual_evapotranspiration_crop_life = self.var.load_initial(
+            "actual_evapotranspiration_crop_life",
             default=self.var.full_compressed(0, dtype=np.float32, gpu=False),
             gpu=False,
         )
-        self.var.potential_transpiration_crop = self.var.load_initial(
-            "potential_transpiration_crop",
+        self.var.potential_evapotranspiration_crop_life = self.var.load_initial(
+            "potential_evapotranspiration_crop_life",
             default=self.var.full_compressed(0, dtype=np.float32, gpu=False),
             gpu=False,
         )
@@ -1910,6 +1910,17 @@ class CropFarmers(AgentBaseClass):
             assert not np.isnan(yield_ratio).any()
         else:
             yield_ratio = np.full_like(crop_map[harvest], 1, dtype=np.float32)
+        if harvest.sum() > 0:
+            print(
+                "yield_ratio",
+                yield_ratio.mean(),
+                "min",
+                yield_ratio.min(),
+                "max",
+                yield_ratio.max(),
+                "median",
+                np.median(yield_ratio),
+            )
         return yield_ratio
 
     def update_yield_ratio_management(self) -> None:
@@ -2008,8 +2019,8 @@ class CropFarmers(AgentBaseClass):
             # Get yield ratio for the harvested crops
             yield_ratio_per_field = self.get_yield_ratio(
                 harvest,
-                self.var.actual_transpiration_crop,
-                self.var.potential_transpiration_crop,
+                self.var.actual_evapotranspiration_crop_life,
+                self.var.potential_evapotranspiration_crop_life,
                 self.var.crop_map,
             )
             assert (yield_ratio_per_field >= 0).all()
@@ -2123,8 +2134,8 @@ class CropFarmers(AgentBaseClass):
             self.profit_farmer = np.zeros(self.n, dtype=np.float32)
 
         # Reset transpiration values for harvested fields
-        self.var.actual_transpiration_crop[harvest] = 0
-        self.var.potential_transpiration_crop[harvest] = 0
+        self.var.actual_evapotranspiration_crop_life[harvest] = 0
+        self.var.potential_evapotranspiration_crop_life[harvest] = 0
 
         # Update crop and land use maps after harvest
         self.var.crop_map[harvest] = -1

geb/hydrology/landcover.py
@@ -357,6 +357,9 @@ class LandCover(object):
             fn=None,
         )
 
+        if __debug__:
+            BIOAREA = self.var.land_use_type < SEALED
+
         self.var.cropKC[self.var.land_use_type == FOREST] = forest_cropCoefficientNC[
             self.var.land_use_type == FOREST
         ]  # forest
@@ -364,9 +367,16 @@ class LandCover(object):
 
         self.var.cropKC[self.var.land_use_type == GRASSLAND_LIKE] = 0.2
 
-        potential_transpiration, potBareSoilEvap, totalPotET = (
-            self.model.evaporation.step(self.var.ETRef)
-        )
+        (
+            potential_transpiration,
+            potential_bare_soil_evaporation,
+            potential_evapotranspiration,
+        ) = self.model.evaporation.step(self.var.ETRef)
+
+        assert (
+            potential_evapotranspiration
+            <= potential_transpiration + potential_bare_soil_evaporation + 1e-5
+        )[BIOAREA].all()
 
         timer.new_split("PET")
 
@@ -374,15 +384,19 @@ class LandCover(object):
             self.model.interception.step(potential_transpiration)
         )  # first thing that evaporates is the intercepted water.
 
+        assert (
+            potential_evapotranspiration
+            <= potential_transpiration + potential_bare_soil_evaporation + 1e-5
+        )[BIOAREA].all()
+
         timer.new_split("Interception")
 
-        # *********  WATER Demand   *************************
         (
             groundwater_abstraction_m3,
             channel_abstraction_m,
-            addtoevapotrans,
             returnFlow,
-        ) = self.model.water_demand.step(totalPotET)
+        ) = self.model.water_demand.step(potential_evapotranspiration)
+
         timer.new_split("Demand")
 
         openWaterEvap = self.var.full_compressed(0, dtype=np.float32)
@@ -401,8 +415,8 @@ class LandCover(object):
             capillar,
             openWaterEvap,
             potential_transpiration_minus_interception_evaporation,
-            potBareSoilEvap,
-            totalPotET,
+            potential_bare_soil_evaporation,
+            potential_evapotranspiration,
         )
         assert not (directRunoff < 0).any()
         timer.new_split("Soil")
@@ -411,13 +425,17 @@ class LandCover(object):
             capillar, openWaterEvap, directRunoff
         )
         assert not (directRunoff < 0).any()
+
         timer.new_split("Sealed")
 
-        self.var.actual_transpiration_crop[self.var.crop_map != -1] += (
-            actual_total_transpiration[self.var.crop_map != -1]
+        self.var.actual_evapotranspiration_crop_life[self.var.crop_map != -1] += (
+            np.minimum(
+                self.var.actual_evapotranspiration[self.var.crop_map != -1],
+                potential_evapotranspiration[self.var.crop_map != -1],
+            )
         )
-        self.var.potential_transpiration_crop[self.var.crop_map != -1] += (
-            potential_transpiration[self.var.crop_map != -1]
+        self.var.potential_evapotranspiration_crop_life[self.var.crop_map != -1] += (
+            potential_evapotranspiration[self.var.crop_map != -1]
         )
 
         assert not (directRunoff < 0).any()

geb/hydrology/soil.py
@@ -1190,8 +1190,8 @@ class Soil(object):
         timer.new_split("Percolation")
 
         bioarea = np.where(self.var.land_use_type < SEALED)[0].astype(np.int32)
-        self.var.actual_evapotranspiration_total[bioarea] = (
-            self.var.actual_evapotranspiration_total[bioarea]
+        self.var.actual_evapotranspiration[bioarea] = (
+            self.var.actual_evapotranspiration[bioarea]
             + actual_bare_soil_evaporation[bioarea]
             + open_water_evaporation[bioarea]
             + actual_total_transpiration[bioarea]
@@ -1240,7 +1240,7 @@ class Soil(object):
                     direct_runoff[bioarea],
                     interflow[bioarea],
                     groundwater_recharge[bioarea],
-                    self.var.actual_evapotranspiration_total[bioarea],
+                    self.var.actual_evapotranspiration[bioarea],
                 ],
                 prestorages=[
                     w_pre[:, bioarea].sum(axis=0),
@@ -1252,6 +1252,20 @@ class Soil(object):
                 ],
                 tollerance=1e-6,
             )
+
+            assert (
+                actual_total_transpiration[bioarea]
+                <= potential_transpiration[bioarea] + 1e-5
+            ).all()
+            assert (
+                actual_bare_soil_evaporation[bioarea]
+                <= potential_bare_soil_evaporation[bioarea] + 1e-5
+            ).all()
+            assert (
+                actual_total_transpiration[bioarea]
+                + actual_bare_soil_evaporation[bioarea]
+                <= potential_evapotranspiration[bioarea] + 1e-5
+            ).all()
 
         timer.new_split("Finalizing")
         if self.model.timing:

geb/hydrology/water_demand.py
@@ -71,7 +71,7 @@ class WaterDemand:
             self.model.data.grid.full_compressed(0, dtype=np.float32),
         )
 
-    def get_potential_irrigation_consumption(self, totalPotET):
+    def get_potential_irrigation_consumption(self, potential_evapotranspiration):
         """Calculate the potential irrigation consumption. Not that consumption
         is not the same as withdrawal. Consumption is the amount of water that
         is actually used by the farmers, while withdrawal is the amount of water
@@ -103,7 +103,7 @@ class WaterDemand:
         # p is between 0 and 1 => if p =1 wcrit = wwp, if p= 0 wcrit = wfc
         p = get_fraction_easily_available_soil_water(
             crop_group_number[nonpaddy_irrigated_land],
-            totalPotET[nonpaddy_irrigated_land],
+            potential_evapotranspiration[nonpaddy_irrigated_land],
         )
 
         root_ratios = get_root_ratios(
@@ -199,7 +199,7 @@ class WaterDemand:
         demand -= withdrawal  # update in place
         return withdrawal
 
-    def step(self, totalPotET):
+    def step(self, potential_evapotranspiration):
         timer = TimingModule("Water demand")
 
         domestic_water_demand, domestic_water_efficiency = (
@@ -218,7 +218,7 @@ class WaterDemand:
             critical_water_level,
             max_water_content,
             potential_infiltration_capacity,
-        ) = self.get_potential_irrigation_consumption(totalPotET)
+        ) = self.get_potential_irrigation_consumption(potential_evapotranspiration)
 
         assert (domestic_water_demand >= 0).all()
         assert (industry_water_demand >= 0).all()
@@ -330,10 +330,10 @@ class WaterDemand:
             self.var.actual_irrigation_consumption = cp.asarray(
                 irrigation_water_consumption_m
             )
-            addtoevapotrans = cp.asarray(addtoevapotrans_m)
+            addtoevapotrans_m = cp.asarray(addtoevapotrans_m)
         else:
             self.var.actual_irrigation_consumption = irrigation_water_consumption_m
-            addtoevapotrans = addtoevapotrans_m
+            addtoevapotrans_m = addtoevapotrans_m
 
         assert (self.var.actual_irrigation_consumption + 1e-6 >= 0).all()
 
@@ -404,6 +404,5 @@ class WaterDemand:
         return (
             groundwater_abstraction_m3,
             channel_abstraction_m3 / self.model.data.grid.cellArea,
-            addtoevapotrans,
             returnFlow,
         )