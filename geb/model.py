dceeb950808f7ee7f7829d50329e72f173be5aae
Jens de Bruijn <j.a.debruijn@outlook.com>
2025-01-27 16:02:12 +0100

    clean report model at start of run

--- geb/model.py
+++ geb/model.py
@@ -1,4 +1,5 @@
 import datetime
+import shutil
 from pathlib import Path
 import geopandas as gpd
 from typing import Union
@@ -120,6 +121,8 @@ class GEBModel(HazardDriver, ABM, Hydrology):
         self.report_folder = (
             Path(self.config["general"]["report_folder"]) / self.run_name
         )
+        # clean report model at start of run
+        shutil.rmtree(self.report_folder, ignore_errors=True)
         self.report_folder.mkdir(parents=True, exist_ok=True)
 
         self.spinup_start = datetime.datetime.combine(
