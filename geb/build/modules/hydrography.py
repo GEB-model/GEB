498acadb8afc6e8af7efc85ecf3182a22cc1286b
Jens de Bruijn <j.a.debruijn@outlook.com>
2025-04-09 14:14:20 +0200

    include coastal areas (default) when setting up a model. Currently this includes all coastal areas between basin outlets

--- geb/build/modules/hydrography.py
+++ geb/build/modules/hydrography.py
@@ -1,23 +1,143 @@
 import geopandas as gpd
+import networkx as nx
 import numpy as np
 import pandas as pd
 import pyflwdir
 import xarray as xr
 from hydromt.exceptions import NoDataException
 from pyflwdir import FlwdirRaster
+from rasterio.features import rasterize
 from scipy.ndimage import value_indices
+from shapely.geometry import LineString
 
 from geb.hydrology.lakes_reservoirs import LAKE, RESERVOIR
 
-from ..workflows.hydrography import (
-    create_river_raster_from_river_lines,
-    get_downstream_subbasins,
-    get_rivers,
-    get_subbasins_geometry,
-    get_SWORD_river_widths,
-    get_SWORD_translation_IDs_and_lenghts,
-    get_upstream_subbasin_ids,
-)
+
+def get_upstream_subbasin_ids(river_graph, subbasin_ids):
+    ancenstors = set()
+    for subbasin_id in subbasin_ids:
+        ancenstors |= nx.ancestors(river_graph, subbasin_id)
+    return ancenstors
+
+
+def get_downstream_subbasins(river_graph, sink_subbasin_ids):
+    downstream_subbasins = {}
+    for subbasin_id in sink_subbasin_ids:
+        downstream_subbasin = list(river_graph.neighbors(subbasin_id))
+        if len(downstream_subbasin) == 0:
+            pass
+        else:
+            assert len(downstream_subbasin) == 1, (
+                "A subbasin has more than one downstream subbasin"
+            )
+            downstream_subbasin = downstream_subbasin[0]
+            if downstream_subbasin not in downstream_subbasins:
+                downstream_subbasins[downstream_subbasin] = []
+            downstream_subbasins[downstream_subbasin].append(subbasin_id)
+
+    return downstream_subbasins
+
+
+def get_subbasins_geometry(data_catalog, subbasin_ids):
+    subbasins = gpd.read_parquet(
+        data_catalog.get_source("MERIT_Basins_cat").path,
+        filters=[
+            ("COMID", "in", subbasin_ids),
+        ],
+    )
+    assert len(subbasins) == len(subbasin_ids), "Some subbasins were not found"
+    return subbasins.set_index("COMID")
+
+
+def get_rivers(data_catalog, subbasin_ids):
+    rivers = gpd.read_parquet(
+        data_catalog.get_source("MERIT_Basins_riv").path,
+        columns=["COMID", "lengthkm", "uparea", "maxup", "geometry"],
+        filters=[
+            ("COMID", "in", subbasin_ids),
+        ],
+    )
+    assert len(rivers) == len(subbasin_ids), "Some rivers were not found"
+    # reverse the river lines to have the downstream direction
+    rivers["geometry"] = rivers["geometry"].apply(
+        lambda x: LineString(list(x.coords)[::-1])
+    )
+    return rivers.set_index("COMID")
+
+
+def create_river_raster_from_river_lines(
+    rivers, flwdir_idxs_out, original_d8_elevation
+):
+    river_raster = rasterize(
+        zip(rivers.geometry, rivers.index),
+        out_shape=original_d8_elevation.shape,
+        fill=-1,
+        dtype=np.int32,
+        transform=original_d8_elevation.rio.transform(),
+        all_touched=False,  # because this is a line, Bresenham's line algorithm is used, which is perfect here :-)
+    )
+
+    river_raster_coarsened = river_raster.ravel()[
+        flwdir_idxs_out.values.ravel()
+    ].reshape(flwdir_idxs_out.shape)
+    return river_raster_coarsened
+
+
+def get_SWORD_translation_IDs_and_lenghts(data_catalog, rivers):
+    MERIT_Basins_to_SWORD = data_catalog.get_source("MERIT_Basins_to_SWORD").path
+    SWORD_files = []
+    for SWORD_region in (
+        list(range(11, 19))
+        + list(range(21, 30))
+        + list(range(31, 37))
+        + list(range(41, 50))
+        + list(range(51, 58))
+        + list(range(61, 68))
+        + list(range(71, 79))
+        + list(range(81, 87))
+        + [91]
+    ):
+        SWORD_files.append(MERIT_Basins_to_SWORD.format(SWORD_Region=str(SWORD_region)))
+    assert len(SWORD_files) == 61, "There are 61 SWORD regions"
+    MERIT_Basins_to_SWORD = (
+        xr.open_mfdataset(SWORD_files).sel(mb=rivers.index.tolist()).compute()
+    )
+
+    SWORD_reach_IDs = np.full((40, len(rivers)), dtype=np.int64, fill_value=-1)
+    SWORD_reach_lengths = np.full(
+        (40, len(rivers)), dtype=np.float64, fill_value=np.nan
+    )
+    for i in range(1, 41):
+        SWORD_reach_IDs[i - 1] = np.where(
+            MERIT_Basins_to_SWORD[f"sword_{i}"] != 0,
+            MERIT_Basins_to_SWORD[f"sword_{i}"],
+            -1,
+        )
+        SWORD_reach_lengths[i - 1] = MERIT_Basins_to_SWORD[f"part_len_{i}"]
+
+    return SWORD_reach_IDs, SWORD_reach_lengths
+
+
+def get_SWORD_river_widths(data_catalog, SWORD_reach_IDs):
+    # NOTE: To open SWORD NetCDFs: xr.open_dataset("oc_sword_v16.nc", group="reaches/discharge_models/constrained/MOMMA")
+    unique_SWORD_reach_ids = np.unique(SWORD_reach_IDs[SWORD_reach_IDs != -1])
+
+    SWORD = gpd.read_file(
+        data_catalog.get_source("SWORD").path,
+        sql=f"""SELECT * FROM sword_reaches_v16 WHERE reach_id IN ({",".join([str(ID) for ID in unique_SWORD_reach_ids])})""",
+    ).set_index("reach_id")
+
+    assert len(SWORD) == len(unique_SWORD_reach_ids), (
+        "Some SWORD reaches were not found, possibly the SWORD and MERIT data version are not correct"
+    )
+
+    def lookup_river_width(reach_id):
+        if reach_id == -1:  # when no SWORD reach is found
+            return np.nan  # return NaN
+        return SWORD.loc[reach_id, "width"]
+
+    SWORD_river_width = np.vectorize(lookup_river_width)(SWORD_reach_IDs)
+    return SWORD_river_width
 
 
 class Hydrography:
@@ -71,6 +191,12 @@ class Hydrography:
         subbasins["associated_upstream_basins"] = pd.Series(
             downstream_subbasins.values(), index=downstream_subbasins
         ).reindex(subbasins.index, fill_value=[])
+
+        # for each basin check if the basin has any downstream neighbors
+        # if not, it is a coastal basin
+        subbasins["is_coastal_basin"] = subbasins.apply(
+            lambda row: len(list(river_graph.neighbors(row.name))) == 0, axis=1
+        )
 
         self.set_geoms(subbasins, name="routing/subbasins")
 
