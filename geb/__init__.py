b4e0acd4bb54802383dda217a24ba2881190aa0c
Jens de Bruijn <j.a.debruijn@outlook.com>
2025-05-12 11:00:11 +0200

    revert change to reasonable_default_config

--- geb/__init__.py
+++ geb/__init__.py
@@ -62,8 +62,6 @@ load_numba_threading_layer()
 xr.set_options(use_bottleneck=False, keep_attrs=True)
 
 # set environment variable for GEB package directory
-os.environ["GEB_PACKAGE_DIR"] = str(
-    Path(importlib.util.find_spec("geb").origin).parent.parent
-)
+os.environ["GEB_PACKAGE_DIR"] = str(Path(__file__).parent)
 
 faulthandler.enable()
