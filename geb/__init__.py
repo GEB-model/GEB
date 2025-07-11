f6a9e8d330eacc6fab8fbc05168840d3057f384c
Jens de Bruijn <j.a.debruijn@outlook.com>
2025-07-11 08:57:20 +0200

    bump version beta

--- geb/__init__.py
+++ geb/__init__.py
@@ -1,6 +1,6 @@
 """GEB simulates the environment, the individual behaviour of people, households and organizations - including their interactions - at small and large scale."""
 
-__version__ = "1.0.0b5"
+__version__ = "1.0.0b6"
 
 import faulthandler
 import os
