457800e22c2966a7c4f8a73afffdb9504ef2b5dc
Jens de Bruijn <j.a.debruijn@outlook.com>
2025-08-15 12:56:59 +0200

    simplify check

--- geb/agents/crop_farmers.py
+++ geb/agents/crop_farmers.py
@@ -791,16 +791,9 @@ class CropFarmers(AgentBaseClass):
         )
 
         if (
-            self.config["expected_utility"]["insurance"]["personal_insurance"][
-                "ruleset"
-            ]
-            != "no-adaptation"
-            or self.config["expected_utility"]["insurance"]["index_insurance"][
-                "ruleset"
-            ]
-            != "no-adaptation"
-            or self.config["expected_utility"]["insurance"]["pr_insurance"]["ruleset"]
-            != "no-adaptation"
+            self.personal_insurance_adaptation_active
+            or self.index_insurance_adaptation_active
+            or self.pr_insurance_adaptation_active
         ):
             for i, varname in enumerate(["pr_gev_c", "pr_gev_loc", "pr_gev_scale"]):
                 GEV_pr_grid = getattr(self.grid, varname)
