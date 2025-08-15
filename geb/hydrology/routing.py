b7cccae8e8a88f092014d8dbdb764b5eb7bdada4
Jens de Bruijn <j.a.debruijn@outlook.com>
2025-08-15 16:21:26 +0200

    remove cwatm header now that files are unrecognizably different ;-)

--- geb/hydrology/routing.py
+++ geb/hydrology/routing.py
@@ -1,25 +1,3 @@
-# --------------------------------------------------------------------------------
-# This file contains code that has been adapted from an original source available
-# in a public repository under the GNU General Public License. The original code
-# has been modified to fit the specific needs of this project.
-#
-# Original source repository: https://github.com/iiasa/CWatM
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# --------------------------------------------------------------------------------
-
-
 import numpy as np
 import numpy.typing as npt
 import pyflwdir
