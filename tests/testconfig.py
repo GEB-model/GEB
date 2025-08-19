eb318b3e796d6b5c38cdec13a351d9dfedb7d9e7
Jens de Bruijn <j.a.debruijn@outlook.com>
2025-08-19 12:15:30 +0200

    use consistent location of test files

--- tests/testconfig.py
+++ tests/testconfig.py
@@ -1,10 +1,19 @@
 import os
 from pathlib import Path
 
-output_folder: Path = Path("tests/output")
+# Get the GEB package directory from the environment variable
+GEB_PACKAGE_DIR_ENV: str | None = os.environ.get("GEB_PACKAGE_DIR", None)
+if GEB_PACKAGE_DIR_ENV is None:
+    raise RuntimeError("GEB_PACKAGE_DIR environment variable is not set.")
+
+GEB_PACKAGE_DIR: Path = Path(GEB_PACKAGE_DIR_ENV)
+
+# Ensure the testing files end in up in the tests folder of the GEB package directory
+output_folder: Path = GEB_PACKAGE_DIR / "tests" / "output"
 output_folder.mkdir(exist_ok=True)
 
-tmp_folder: Path = Path("tests/tmp")
+tmp_folder: Path = GEB_PACKAGE_DIR / "tests" / "tmp"
 tmp_folder.mkdir(exist_ok=True)
 
+# This flag is used to turn on or off test cases in the Github Actions environment.
 IN_GITHUB_ACTIONS: bool = os.getenv("GITHUB_ACTIONS") == "true"
