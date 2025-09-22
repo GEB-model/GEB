d67e452ebea1e07a0ad815c8d69a02185cb0647b
Jens de Bruijn <2176353+jensdebruijn@users.noreply.github.com>
2025-09-22 15:00:23 +0200

    use new data catalog

--- geb/build/modules/agents.py
+++ geb/build/modules/agents.py
@@ -353,9 +353,8 @@ class Agents:
         cols_to_keep = ["REF_AREA", "STATISTICAL_OPERATION", "TIME_PERIOD", "OBS_VALUE"]
         oecd_idd = oecd_idd[cols_to_keep]
         # only done to check countries in region, could probably be done more efficiently
-        countries = self.data_catalog.get_geodataframe(
-            "GADM_level0",
-            geom=self.region,
+        countries = self.new_data_catalog.get("GADM_level0").read(
+            geom=self.region.union_all(),
         )
         # setup donor countries for country missing in oecd data
         donor_countries = setup_donor_countries(self, oecd_idd["REF_AREA"])
