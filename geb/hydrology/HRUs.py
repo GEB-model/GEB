fix issues with negative hourly values for PET calculation (should become dew in a future update), make evaporation in rivers and water bodies also hourly

geb/hydrology/HRUs.py
@@ -14,6 +14,7 @@ from affine import Affine
 from numba import njit
 from scipy.spatial import cKDTree
 
+from geb.typing import ArrayFloat32, TwoDFloatArrayFloat32
 from geb.workflows.io import load_grid, open_zarr
 from geb.workflows.raster import compress
 
@@ -271,7 +272,7 @@ class Grid(BaseVariables):
 
         BaseVariables.__init__(self)
 
-    def full(self, *args: Any, **kwargs: Any) -> np.ndarray:
+    def full(self, *args: Any, **kwargs: Any) -> TwoDFloatArrayFloat32:
         """Return a full array with size of mask. Takes any other argument normally used in np.full.
 
         Args:
@@ -283,7 +284,7 @@ class Grid(BaseVariables):
         """
         return np.full(self.mask.shape, *args, **kwargs)
 
-    def full_compressed(self, *args: Any, **kwargs: Any) -> np.ndarray:
+    def full_compressed(self, *args: Any, **kwargs: Any) -> ArrayFloat32:
         """Return a full array with size of compressed array. Takes any other argument normally used in np.full.
 
         Args:

geb/hydrology/__init__.py
@@ -117,6 +117,7 @@ class Hydrology(Data, Module):
         timer.finish_split("Waterbodies")
 
         (
+            reference_evapotranspiration_water_m,
             interflow_m,
             runoff_m,
             groundwater_recharge_m,
@@ -215,7 +216,10 @@ class Hydrology(Data, Module):
         timer.finish_split("Runoff concentration")
 
         routing_loss_m3, over_abstraction_m3 = self.routing.step(
-            self.grid.var.total_runoff_m, channel_abstraction_m3, return_flow_m
+            self.grid.var.total_runoff_m,
+            channel_abstraction_m3,
+            return_flow_m,
+            reference_evapotranspiration_water_m,
         )
 
         current_storage: np.float64 = self.get_current_storage()

geb/hydrology/landsurface.py
@@ -9,7 +9,7 @@ from geb.module import Module
 from geb.workflows import balance_check
 from geb.workflows.io import load_grid
 
-from .evapotranspiration import (  # noqa: F401
+from .evapotranspiration import (
     calculate_bare_soil_evaporation,
     calculate_transpiration,
 )
@@ -166,12 +166,12 @@ def land_surface_model(
         saturated_hydraulic_conductivity_m_per_s
     ) * 3600.0
 
-    reference_evapotranspiration_grass_m = np.zeros_like(pr_kg_per_m2_per_s)
-    reference_evapotranspiration_water_m = np.zeros_like(pr_kg_per_m2_per_s)
     runoff_m = np.zeros_like(pr_kg_per_m2_per_s)
     interflow_m = np.zeros_like(pr_kg_per_m2_per_s)
+    reference_evapotranspiration_water_m = np.zeros_like(pr_kg_per_m2_per_s)
 
     # total per day variables for water balance
+    reference_evapotranspiration_grass_m = np.zeros_like(snow_water_equivalent_m)
     rain_m = np.zeros_like(snow_water_equivalent_m)
     snow_m = np.zeros_like(snow_water_equivalent_m)
     sublimation_m = np.zeros_like(snow_water_equivalent_m)
@@ -205,8 +205,8 @@ def land_surface_model(
             )  # Wind speed at 10m height
 
             (
-                reference_evapotranspiration_grass_m[hour, i],
-                reference_evapotranspiration_water_m[hour, i],
+                reference_evapotranspiration_grass_m_hour_cell,
+                reference_evapotranspiration_water_m_hour_cell,
                 net_absorbed_radiation_vegetation_MJ_m2_dt,
                 actual_vapour_pressure_Pa,
             ) = get_reference_evapotranspiration(
@@ -216,6 +216,22 @@ def land_surface_model(
                 rlds_W_per_m2=rlds_W_per_m2_cell[hour],
                 rsds_W_per_m2=rsds_W_per_m2_cell[hour],
                 wind_10m_m_per_s=wind_10m_m_per_s,
+            )
+
+            # Ensure non-negative evapotranspiration values
+            # TODO: Use this to implement dew rather than clipping
+            reference_evapotranspiration_grass_m_hour_cell = max(
+                reference_evapotranspiration_grass_m_hour_cell, np.float32(0.0)
+            )
+            reference_evapotranspiration_water_m_hour_cell = max(
+                reference_evapotranspiration_water_m_hour_cell, np.float32(0.0)
+            )
+
+            reference_evapotranspiration_grass_m[i] += (
+                reference_evapotranspiration_grass_m_hour_cell
+            )
+            reference_evapotranspiration_water_m[hour, i] += (
+                reference_evapotranspiration_water_m_hour_cell
             )
 
             (
@@ -254,15 +270,13 @@ def land_surface_model(
 
             potential_bare_soil_evaporation_m: np.float32 = (
                 get_potential_bare_soil_evaporation(
-                    reference_evapotranspiration_grass_m[hour, i],
+                    reference_evapotranspiration_grass_m_hour_cell,
                     sublimation_m_cell_hour,
                 )
             )
 
             potential_evapotranspiration_m: np.float32 = get_potential_evapotranspiration(
-                reference_evapotranspiration_grass_m=reference_evapotranspiration_grass_m[
-                    hour, i
-                ],
+                reference_evapotranspiration_grass_m=reference_evapotranspiration_grass_m_hour_cell,
                 crop_factor=crop_factor[i],
                 CO2_induced_crop_factor_adustment=CO2_induced_crop_factor_adustment,
             )
@@ -297,9 +311,7 @@ def land_surface_model(
                     natural_available_water_infiltration_m=natural_available_water_infiltration_m,
                     actual_irrigation_consumption_m=actual_irrigation_consumption_m[i],
                     land_use_type=land_use_type[i],
-                    reference_evapotranspiration_water_m_per_day=reference_evapotranspiration_water_m[
-                        hour, i
-                    ],
+                    reference_evapotranspiration_water_m=reference_evapotranspiration_water_m_hour_cell,
                     topwater_m=topwater_m[i],
                 )
             )
@@ -761,6 +773,7 @@ class LandSurface(Module):
         npt.NDArray[np.float32],
         npt.NDArray[np.float32],
         npt.NDArray[np.float32],
+        npt.NDArray[np.float32],
     ]:
         """Step function for the land surface module.
 
@@ -897,8 +910,8 @@ class LandSurface(Module):
             rain_m,
             snow_m,
             self.HRU.var.topwater_m,
-            reference_evapotranspiration_grass_m_dt,
-            reference_evapotranspiration_water_m_dt,
+            reference_evapotranspiration_grass_m,
+            reference_evapotranspiration_water_m,
             self.HRU.var.snow_water_equivalent_m,
             self.HRU.var.liquid_water_in_snow_m,
             sublimation_or_deposition_m,
@@ -1029,26 +1042,19 @@ class LandSurface(Module):
             crop_sub_stage[growing_crop_mask], growing_crop_mask
         ] += potential_transpiration_m[growing_crop_mask]
 
-        self.HRU.var.reference_evapotranspiration_water_m_per_day = np.sum(
-            reference_evapotranspiration_water_m_dt, axis=0
+        reference_evapotranspiration_water_m = self.hydrology.to_grid(
+            HRU_data=reference_evapotranspiration_water_m,
+            fn="weightedmean",
         )
-        self.HRU.var.reference_evapotranspiration_grass_m_per_day = np.sum(
-            reference_evapotranspiration_grass_m_dt, axis=0
-        )
-
-        self.grid.var.reference_evapotranspiration_water_m_per_day = (
-            self.hydrology.to_grid(
-                HRU_data=self.HRU.var.reference_evapotranspiration_water_m_per_day,
-                fn="weightedmean",
-            )
-        )
+        assert (reference_evapotranspiration_water_m >= 0).all()
 
         self.model.agents.crop_farmers.save_water_deficit(
-            self.HRU.var.reference_evapotranspiration_grass_m_per_day
+            reference_evapotranspiration_grass_m
         )
         self.report(locals())
 
         return (
+            reference_evapotranspiration_water_m,
             interflow_m,
             runoff_m,
             groundwater_recharge_m,

geb/hydrology/routing.py
@@ -8,6 +8,7 @@ import pyflwdir
 from numba import njit
 
 from geb.module import Module
+from geb.typing import ArrayFloat32
 from geb.workflows import balance_check
 
 
@@ -891,7 +892,7 @@ class Routing(Module):
                 self.default_missing_channel_width,
             )
             self.router = KinematicWave(
-                dt=self.var.routing_step_length_seconds,
+                dt=3600,
                 river_network=self.river_network,
                 Q_initial=self.grid.var.discharge_m3_s_substep,
                 river_width=river_width,
@@ -903,7 +904,7 @@ class Routing(Module):
             )
         elif routing_algorithm == "accuflux":
             self.router = Accuflux(
-                dt=self.var.routing_step_length_seconds,
+                dt=3600,
                 river_network=self.river_network,
                 Q_initial=self.grid.var.discharge_m3_s_substep,
                 waterbody_id=self.grid.var.waterBodyID,
@@ -930,10 +931,7 @@ class Routing(Module):
             self.model.files["grid"]["routing/upstream_area"]
         )
 
-        # number of substep per day
-        self.var.n_routing_substeps = 24
         # kinematic wave parameter: 0.6 is for broad sheet flow
-
         self.var.river_beta = 0.6  # TODO: Make this a parameter
 
         # Channel Manning's n
@@ -960,11 +958,6 @@ class Routing(Module):
         # Channel bottom width [meters]
         self.grid.var.average_river_width = self.grid.load(
             self.model.files["grid"]["routing/river_width"]
-        )
-
-        # Corresponding sub-timestep (seconds)
-        self.var.routing_step_length_seconds = (
-            self.model.timestep_length.total_seconds() / self.var.n_routing_substeps
         )
 
         # for a river, the wetted perimeter can be approximated by the channel width
@@ -994,7 +987,7 @@ class Routing(Module):
             1e-30, dtype=np.float32
         )
         self.grid.var.discharge_m3_s_per_substep = np.full(
-            (self.var.n_routing_substeps, self.grid.var.discharge_m3_s_substep.size),
+            (24, self.grid.var.discharge_m3_s_substep.size),
             0,
             dtype=self.grid.var.discharge_m3_s_substep.dtype,
         )
@@ -1035,7 +1028,7 @@ class Routing(Module):
             self.grid.var.average_river_width, beta, dtype=np.float32
         )
         # for the first year of simulation, we use the default alpha value for all rivers
-        if self.var.discharge_step_count < 365 * self.var.n_routing_substeps:
+        if self.var.discharge_step_count < 365 * 24:
             alpha: npt.NDArray[np.float32] = np.full_like(
                 self.grid.var.average_river_width,
                 default_alpha,
@@ -1061,6 +1054,7 @@ class Routing(Module):
         total_runoff_m: npt.NDArray[np.float32],
         channel_abstraction_m3: npt.NDArray[np.float32],
         return_flow: npt.NDArray[np.float32],
+        reference_evapotranspiration_water_m: npt.NDArray[np.float32],
     ) -> tuple[
         np.float32,
         np.float32,
@@ -1069,9 +1063,10 @@ class Routing(Module):
 
         Args:
             total_runoff_m: Total runoff in meters for each grid cell for each hour.
-                Shape is (n_routing_substeps, n_cells).
+                Shape is (24, n_cells).
             channel_abstraction_m3: Channel abstraction in m3 for each grid cell over the whole day.
             return_flow: Return flow in meters for each grid cell over the whole day.
+            reference_evapotranspiration_water_m: Reference evapotranspiration from water in meters for for each grid cell for each hour.
 
         Returns:
             A tuple containing:
@@ -1087,56 +1082,46 @@ class Routing(Module):
                 self.router.get_total_storage()
             )
 
-        channel_abstraction_m3_per_routing_step: np.ndarray = (
-            channel_abstraction_m3 / self.var.n_routing_substeps
-        )
+        channel_abstraction_m3_per_hour: np.ndarray = channel_abstraction_m3 / 24
         assert (
-            channel_abstraction_m3_per_routing_step[self.grid.var.waterBodyID != -1]
-            == 0.0
+            channel_abstraction_m3_per_hour[self.grid.var.waterBodyID != -1] == 0.0
         ).all(), (
             "Channel abstraction must be zero for water bodies, "
             "but found non-zero value."
         )
 
-        return_flow_m3_per_routing_step: np.ndarray = (
-            return_flow * self.grid.var.cell_area / self.var.n_routing_substeps
-        )
+        return_flow_m3_per_hour: np.ndarray = return_flow * self.grid.var.cell_area / 24
 
         # add return flow to the water bodies
-        return_flow_m3_to_water_bodies_per_routing_step: np.ndarray = np.bincount(
+        return_flow_m3_to_water_bodies_per_hour: np.ndarray = np.bincount(
             self.grid.var.waterBodyID[self.grid.var.waterBodyID != -1],
-            weights=return_flow_m3_per_routing_step[self.grid.var.waterBodyID != -1],
+            weights=return_flow_m3_per_hour[self.grid.var.waterBodyID != -1],
         )
-        return_flow_m3_per_routing_step[self.grid.var.waterBodyID != -1] = 0.0
+        return_flow_m3_per_hour[self.grid.var.waterBodyID != -1] = 0.0
 
         self.grid.var.discharge_m3_s_per_substep = np.full(
-            (self.var.n_routing_substeps, self.grid.var.discharge_m3_s_substep.size),
+            (24, self.grid.var.discharge_m3_s_substep.size),
             np.nan,
             dtype=self.grid.var.discharge_m3_s_substep.dtype,
         )
 
-        potential_evaporation_per_water_body_m3_per_routing_step = (
-            np.bincount(
-                self.grid.var.waterBodyID[self.grid.var.waterBodyID != -1],
-                weights=self.grid.var.reference_evapotranspiration_water_m_per_day[
-                    self.grid.var.waterBodyID != -1
-                ],
-            )
-            / np.bincount(self.grid.var.waterBodyID[self.grid.var.waterBodyID != -1])
-            * self.hydrology.lakes_reservoirs.var.lake_area
-        ) / self.var.n_routing_substeps
-
         if __debug__:
             # these are for balance checks, the sum of all routing steps
-            evaporation_in_rivers_m3 = 0
-            waterbody_evaporation_m3 = 0
-            outflow_at_pits_m3 = 0
-            over_abstraction_m3 = 0
-            command_area_release_m3 = 0
+            evaporation_in_rivers_m3: ArrayFloat32 = self.grid.full_compressed(
+                0, dtype=np.float32
+            )
+            waterbody_evaporation_m3: ArrayFloat32 = np.zeros(
+                self.hydrology.lakes_reservoirs.n, dtype=np.float32
+            )
+            outflow_at_pits_m3 = np.float32(0)
+            over_abstraction_m3: ArrayFloat32 = self.grid.full_compressed(
+                0, dtype=np.float32
+            )
+            command_area_release_m3 = np.float32(0)
 
-        for subrouting_step in range(self.var.n_routing_substeps):
+        for hour in range(24):
             total_runoff_m3: np.ndarray = (
-                total_runoff_m[subrouting_step, :] * self.grid.var.cell_area
+                total_runoff_m[hour, :] * self.grid.var.cell_area
             )
 
             # then split the runoff into runoff directly to water bodies
@@ -1151,23 +1136,37 @@ class Routing(Module):
             total_runoff_m3[self.grid.var.waterBodyID != -1] = 0.0
 
             self.hydrology.lakes_reservoirs.var.storage += (
-                return_flow_m3_to_water_bodies_per_routing_step
+                return_flow_m3_to_water_bodies_per_hour
             )
 
-            actual_evaporation_from_water_bodies_per_routing_step_m3 = np.minimum(
-                potential_evaporation_per_water_body_m3_per_routing_step,
+            # TODO: This calculation can be optimized by pre-calculating some parts
+            potential_evaporation_per_water_body_m3 = (
+                np.bincount(
+                    self.grid.var.waterBodyID[self.grid.var.waterBodyID != -1],
+                    weights=reference_evapotranspiration_water_m[
+                        hour, self.grid.var.waterBodyID != -1
+                    ],
+                )
+                / np.bincount(
+                    self.grid.var.waterBodyID[self.grid.var.waterBodyID != -1]
+                )
+                * self.hydrology.lakes_reservoirs.var.lake_area
+            )
+
+            actual_evaporation_from_water_bodies_per_hour_m3 = np.minimum(
+                potential_evaporation_per_water_body_m3,
                 self.hydrology.lakes_reservoirs.var.storage,
             )
 
             self.hydrology.lakes_reservoirs.var.storage -= (
-                actual_evaporation_from_water_bodies_per_routing_step_m3
+                actual_evaporation_from_water_bodies_per_hour_m3
             )
 
             outflow_per_waterbody_m3, command_area_release_m3_routing_step = (
                 self.hydrology.lakes_reservoirs.substep(
-                    current_substep=subrouting_step,
-                    n_routing_substeps=self.var.n_routing_substeps,
-                    routing_step_length_seconds=self.var.routing_step_length_seconds,
+                    current_substep=hour,
+                    n_routing_substeps=24,
+                    routing_step_length_seconds=3600,
                 )
             )
 
@@ -1179,14 +1178,13 @@ class Routing(Module):
                 outflow_per_waterbody_m3 <= self.hydrology.lakes_reservoirs.var.storage
             ).all(), "outflow cannot be smaller or equal to storage"
 
-            side_flow_channel_m3_per_routing_step = (
+            side_flow_channel_m3_per_hour = (
                 total_runoff_m3
-                + return_flow_m3_per_routing_step
-                - channel_abstraction_m3_per_routing_step
+                + return_flow_m3_per_hour
+                - channel_abstraction_m3_per_hour
             )
             assert (
-                side_flow_channel_m3_per_routing_step[self.grid.var.waterBodyID != -1]
-                == 0
+                side_flow_channel_m3_per_hour[self.grid.var.waterBodyID != -1] == 0
             ).all()
 
             if self.model.in_spinup:
@@ -1217,25 +1215,27 @@ class Routing(Module):
             )
 
             # calculate evaporation from rivers per timestep usting the current channel ratio
-            potential_evaporation_in_rivers_m3_per_routing_step = (
-                self.grid.var.reference_evapotranspiration_water_m_per_day
+            potential_evaporation_in_rivers_m3_per_hour = (
+                reference_evapotranspiration_water_m[hour]
                 * channel_ratio
                 * self.grid.var.cell_area
-            ) / self.var.n_routing_substeps
+            )
 
             (
                 self.grid.var.discharge_m3_s_substep,
-                actual_evaporation_in_rivers_m3_per_routing_step,
+                actual_evaporation_in_rivers_m3_per_hour,
                 over_abstraction_m3_routing_step,
                 self.hydrology.lakes_reservoirs.var.storage,
                 waterbody_inflow_m3,
                 outflow_at_pits_m3_routing_step,
             ) = self.router.step(
-                sideflow_m3=side_flow_channel_m3_per_routing_step.astype(np.float32),
-                evaporation_m3=potential_evaporation_in_rivers_m3_per_routing_step,
+                sideflow_m3=side_flow_channel_m3_per_hour.astype(np.float32),
+                evaporation_m3=potential_evaporation_in_rivers_m3_per_hour,
                 waterbody_storage_m3=self.hydrology.lakes_reservoirs.var.storage,
                 outflow_per_waterbody_m3=outflow_per_waterbody_m3,
             )
+
+            assert (actual_evaporation_in_rivers_m3_per_hour >= 0.0).all()
 
             # the reservoir operators need to track the inflow to the reservoirs
             self.model.agents.reservoir_operators.track_inflow(
@@ -1250,7 +1250,7 @@ class Routing(Module):
             self.var.sum_of_all_discharge_steps += self.grid.var.discharge_m3_s_substep
             self.var.discharge_step_count += 1
 
-            self.grid.var.discharge_m3_s_per_substep[subrouting_step, :] = (
+            self.grid.var.discharge_m3_s_per_substep[hour, :] = (
                 self.grid.var.discharge_m3_s_substep.copy()
             )
 
@@ -1260,11 +1260,9 @@ class Routing(Module):
                 # Discharge at outlets and lakes and reservoirs
                 outflow_at_pits_m3 += outflow_at_pits_m3_routing_step
                 waterbody_evaporation_m3 += (
-                    actual_evaporation_from_water_bodies_per_routing_step_m3
+                    actual_evaporation_from_water_bodies_per_hour_m3
                 )
-                evaporation_in_rivers_m3 += (
-                    actual_evaporation_in_rivers_m3_per_routing_step
-                )
+                evaporation_in_rivers_m3 += actual_evaporation_in_rivers_m3_per_hour
                 over_abstraction_m3 += over_abstraction_m3_routing_step
                 command_area_release_m3 += command_area_release_m3_routing_step
 
@@ -1310,6 +1308,10 @@ class Routing(Module):
             total_outflow_at_pits_m3: np.float64 = outflow_at_pits_m3.astype(
                 np.float64
             ).sum()
+
+            assert total_evaporation_in_rivers_m3 >= 0
+            assert total_waterbody_evaporation_m3 >= 0
+            assert total_outflow_at_pits_m3 >= 0
 
             routing_loss: np.float64 = (
                 total_evaporation_in_rivers_m3

geb/hydrology/soil.py
@@ -15,7 +15,7 @@ def add_water_to_topwater_and_evaporate_open_water(
     natural_available_water_infiltration_m: np.float32,
     actual_irrigation_consumption_m: np.float32,
     land_use_type: np.int32,
-    reference_evapotranspiration_water_m_per_day: np.float32,
+    reference_evapotranspiration_water_m: np.float32,
     topwater_m: np.float32,
 ) -> tuple[np.float32, np.float32]:
     """Add available water from natural and innatural sources to the topwater and calculate open water evaporation.
@@ -24,7 +24,7 @@ def add_water_to_topwater_and_evaporate_open_water(
         natural_available_water_infiltration_m: The natural available water infiltration in m.
         actual_irrigation_consumption_m: The actual irrigation consumption in m.
         land_use_type: The land use type of the hydrological response unit.
-        reference_evapotranspiration_water_m_per_day: The reference evapotranspiration from water in m.
+        reference_evapotranspiration_water_m: The reference evapotranspiration from water in m.
         topwater_m: The topwater in m, which is the water available for evaporation and transpiration.
 
     Returns:
@@ -41,13 +41,13 @@ def add_water_to_topwater_and_evaporate_open_water(
     if land_use_type == PADDY_IRRIGATED:
         open_water_evaporation_m = min(
             max(np.float32(0.0), topwater_m),
-            reference_evapotranspiration_water_m_per_day,
+            reference_evapotranspiration_water_m,
         )
     elif land_use_type == SEALED:
         # evaporation from precipitation fallen on sealed area (ponds)
-        # estimated as 0.2 x reference_evapotranspiration_water_m_per_day
+        # estimated as 0.2 x reference evapotranspiration from water
         open_water_evaporation_m = min(
-            np.float32(0.2) * reference_evapotranspiration_water_m_per_day, topwater_m
+            np.float32(0.2) * reference_evapotranspiration_water_m, topwater_m
         )
     else:
         # no open water evaporation for other land use types (thus using default of 0)

tests/hydrology/test_soil.py
@@ -43,7 +43,7 @@ def test_add_water_to_topwater_and_evaporate_open_water() -> None:
                 natural_available_water_infiltration_m=np.float32(infiltration),
                 actual_irrigation_consumption_m=np.float32(irrigation),
                 land_use_type=np.int32(land_use_type),
-                reference_evapotranspiration_water_m_per_day=np.float32(et_ref),
+                reference_evapotranspiration_water_m=np.float32(et_ref),
                 topwater_m=topwater,
             )
         )
@@ -72,7 +72,7 @@ def test_add_water_to_topwater_and_evaporate_open_water() -> None:
             natural_available_water_infiltration_m=np.float32(0.0),
             actual_irrigation_consumption_m=np.float32(0.0),
             land_use_type=np.int32(PADDY_IRRIGATED),
-            reference_evapotranspiration_water_m_per_day=np.float32(0.0),
+            reference_evapotranspiration_water_m=np.float32(0.0),
             topwater_m=topwater,
         )
     )
@@ -86,7 +86,7 @@ def test_add_water_to_topwater_and_evaporate_open_water() -> None:
             natural_available_water_infiltration_m=np.float32(0.0),
             actual_irrigation_consumption_m=np.float32(0.0),
             land_use_type=np.int32(PADDY_IRRIGATED),
-            reference_evapotranspiration_water_m_per_day=np.float32(
+            reference_evapotranspiration_water_m=np.float32(
                 1.0
             ),  # Much larger than topwater
             topwater_m=topwater,