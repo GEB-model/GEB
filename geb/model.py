62df5bdbde3514d1128afe4c24e00d7402a0c739
Rafaella <rafaellaglo@gmail.com>
2025-11-10 13:29:11 +0100

    uncomment multiverse

--- geb/model.py
+++ geb/model.py
@@ -338,10 +338,10 @@ class GEBModel(Module, HazardDriver, ABM_Model):
                         dt, datetime.time(0)
                     )  # Convert date back to datetime for the multiverse method
 
-                    # self.multiverse(
-                    #     forecast_issue_datetime=forecast_datetime,
-                    #     return_mean_discharge=True,
-                    # )  # run the multiverse for the current timestep
+                    self.multiverse(
+                        forecast_issue_datetime=forecast_datetime,
+                        return_mean_discharge=True,
+                    )  # run the multiverse for the current timestep
 
                     # after the multiverse has run all members for one day, if warning response is enabled, run the warning system
                     if self.config["agent_settings"]["households"]["warning_response"]:
