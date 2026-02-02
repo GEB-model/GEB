d9a6807eda48a4e41308a60e31794867b23e4f0f
Tierolf <lars_tierolf@hotmail.com>
2026-02-02 16:49:21 +0100

    also support gadm28 for compatibility issues

--- geb/build/data_catalog/gadm.py
+++ geb/build/data_catalog/gadm.py
@@ -86,7 +86,7 @@ class GADM(Adapter):
 class GADM28(Adapter):
     """Class for GADM version 2.8 data adapter."""
 
-    def __init__(self, level: int, *args: Any, **kwargs: Any) -> None:
+    def __init__(self, level: int = 1, *args: Any, **kwargs: Any) -> None:
         """Initialize the GADM adapter.
 
         Args:
@@ -105,7 +105,26 @@ class GADM28(Adapter):
         Returns:
             The instance of the Adapter after processing.
         """
-        pass
+        if not self.is_ready:
+            download_path: Path = self.root / url.split(sep="/")[-1]
+            fetch_and_save(url=url, file_path=download_path)
+
+            uncompressed_file: Path = download_path.with_suffix(suffix="")
+            with zipfile.ZipFile(file=download_path, mode="r") as zip_ref:
+                zip_ref.extractall(uncompressed_file)
+            download_path.unlink()  # remove zip file
+            gdf: gpd.GeoDataFrame = gpd.read_file(
+                filename=uncompressed_file / "gadm28" / "gadm28_adm1.shp"
+            )
+            gdf.to_parquet(
+                self.path,
+                engine="pyarrow",
+                compression="gzip",
+                compression_level=9,
+                write_covering_bbox=True,
+            )
+            shutil.rmtree(path=uncompressed_file)  # remove uncompressed folder
+        return self
 
     def read(self, **kwargs: Any) -> gpd.GeoDataFrame:
         """Read the GADM data as a GeoDataFrame.
@@ -115,4 +134,6 @@ class GADM28(Adapter):
         Returns:
             A GeoDataFrame with the GADM data.
         """
-        pass
+        gdf = Adapter.read(self, **kwargs)
+        assert isinstance(gdf, gpd.GeoDataFrame)
+        return gdf
