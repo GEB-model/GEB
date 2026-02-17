bed54b0cd596fd2d54b15a3fe1386b9aedb90a9d
Jens de Bruijn <2176353+jensdebruijn@users.noreply.github.com>
2026-02-17 14:03:56 +0100

    write figures

--- tests/hazards/floods/test_sfincs.py
+++ tests/hazards/floods/test_sfincs.py
@@ -151,6 +151,7 @@ def build_sfincs(
         if "routing/custom_rivers" in geb_model.files["geom"]
         else None,
         overwrite="auto",
+        write_figures=True,
     )
     geb_model.close()
 
