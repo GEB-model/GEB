5fb2b274228c583b344d023f455e4b0065b492ce
Jens de Bruijn <2176353+jensdebruijn@users.noreply.github.com>
2026-02-24 16:57:29 +0100

    fix np divide by 0 error

--- geb/build/modules/hydrography.py
+++ geb/build/modules/hydrography.py
@@ -889,9 +889,14 @@ class Hydrography(BuildModelBase):
             self.data_catalog, SWORD_reach_IDs
         )
 
-        rivers["width"] = np.nansum(
-            SWORD_river_widths * SWORD_reach_lengths, axis=0
-        ) / np.nansum(SWORD_reach_lengths, axis=0)
+        rivers_with_data = (SWORD_reach_IDs != -1).any(axis=0)
+
+        rivers.loc[rivers_with_data, "width"] = (
+            np.nansum(SWORD_river_widths * SWORD_reach_lengths, axis=0)[
+                rivers_with_data
+            ]
+            / np.sum(SWORD_reach_lengths, axis=0)[rivers_with_data]
+        )
         # ensure that all rivers with a SWORD ID have a width
         assert (~np.isnan(rivers["width"][(SWORD_reach_IDs != -1).any(axis=0)])).all()
 
