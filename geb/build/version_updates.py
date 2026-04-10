34d94d8a28f1808717cdd48548f2baab722fdfbe
Tierolf <lars_tierolf@hotmail.com>
2026-04-10 11:25:03 +0200

    move changes to version 20b

--- geb/build/version_updates.py
+++ geb/build/version_updates.py
@@ -12,12 +12,14 @@ if TYPE_CHECKING:
     from geb.build import GEBModel as GEBModelBuild
 
 VERSION_UPDATES: dict[str, list[str]] = {
-    "1.0.0b20": ["[update-python;3.14.4]"],
+    "1.0.0b20": [
+        "[update-python;3.14.4]",
+        "[manual] Add `setup_flood_damage_model` to your `build.yml`.",
+        "[manual] Run `setup_flood_damage_model`: `geb update -b build.yml::setup_flood_damage_model`.",
+    ],
     "1.0.0b19": [
         "[create-file;build_complete.txt]",
         "[update-method;setup_hydrography]",
-        "[manual] Add `setup_flood_damage_model` to your `build.yml`.",
-        "[manual] Run `setup_flood_damage_model`: `geb update -b build.yml::setup_flood_damage_model`.",
     ],
     "1.0.0b18": [
         "[update-method;setup_forcing]",
