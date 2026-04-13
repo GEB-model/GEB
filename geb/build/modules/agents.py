31228899b138a1083bf880a3c894fdf3ba457cdb
Tierolf <lars_tierolf@hotmail.com>
2026-04-13 15:32:37 +0200

    implement distance function to river and coast

--- geb/build/modules/agents.py
+++ geb/build/modules/agents.py
@@ -13,6 +13,7 @@ import xarray as xr
 from dateutil.relativedelta import relativedelta
 from shapely import union_all
 from tqdm import tqdm
+from geb.workflows.methods import get_utm_zone
 
 from geb.build.data_catalog.global_exposure_model import gem_country_name_aliases
 from geb.build.methods import build_method
@@ -1504,6 +1505,35 @@ class Agents(BuildModelBase):
 
         return buildings
 
+    def calculate_distance_parameters(self, buildings: gpd.GeoDataFrame):
+
+        rivers = gpd.read_parquet("input/" + self.files["geom"]["routing/rivers"])
+        coastline = gpd.read_parquet(
+            "input/" + self.files["geom"]["coastal/coastlines"]
+        )
+        region_centroid = self.region.union_all().centroid
+        utm_zone = get_utm_zone(region_centroid.x, region_centroid.y)
+        buildings_crs = buildings.crs
+
+        # Ensure same CRS (projected!)
+        buildings = buildings.to_crs(utm_zone)
+        rivers = rivers.to_crs(utm_zone)
+        coastline = coastline.to_crs(utm_zone)
+
+        # Distance to river
+        buildings["distance_to_river_m"] = buildings.sjoin_nearest(
+            rivers, how="left", distance_col="distance_to_river"
+        )["distance_to_river"].astype(np.int32)
+
+        # Distance to coastline
+        buildings["distance_to_coastline_m"] = buildings.sjoin_nearest(
+            coastline, how="left", distance_col="distance_to_coastline"
+        )["distance_to_coastline"].astype(np.int32)
+
+        # reproject back to original CRS
+        buildings = buildings.to_crs(buildings_crs)
+        return buildings
+
 <<<<<<< HEAD
     def calculate_distance_parameters(
         self, buildings: gpd.GeoDataFrame
@@ -1618,6 +1648,7 @@ class Agents(BuildModelBase):
             geom=mask,
         )
         buildings = self.setup_building_reconstruction_costs(buildings)
+        buildings = self.calculate_distance_parameters(buildings)
 <<<<<<< HEAD
         buildings = self.calculate_distance_parameters(buildings)
 =======
