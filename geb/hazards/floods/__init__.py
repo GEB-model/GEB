66cd8db3d68691b6609564a8a881e34359bac23f
Jens de Bruijn <2176353+jensdebruijn@users.noreply.github.com>
2026-04-15 13:47:34 +0200

    some progress using reported time series rather than grid for sfincs

--- geb/hazards/floods/__init__.py
+++ geb/hazards/floods/__init__.py
@@ -279,7 +279,7 @@ class Floods(Module):
             subbasins=subbasins,
             DEMs=self.DEM_config,
             rivers=rivers,
-            discharge=self.discharge_spinup_ds,
+            discharge=self.discharge_spinup,
             river_width_alpha=self.model.hydrology.grid.decompress(
                 self.model.hydrology.grid.var.river_width_alpha
             ),
@@ -638,7 +638,7 @@ class Floods(Module):
                     coastal=False,
                 )
                 sfincs_inland_root_model.estimate_discharge_for_return_periods(
-                    discharge=self.discharge_spinup_ds,
+                    discharge=self.discharge_spinup,
                     return_periods=self.config["return_periods"],
                     p_value_threshold=self.config["p_value_threshold"],
                     selection_strategy=self.config["selection_strategy"],
@@ -765,34 +765,36 @@ class Floods(Module):
         )  # this is a deque, so it will automatically remove the oldest runoff
 
     @property
-    def discharge_spinup_ds(self) -> xr.DataArray:
+    def discharge_spinup(self) -> pd.DataFrame:
         """Open the discharge datasets from the model output folder.
 
         Returns:
-            The discharge data array after spinup period.
+            A pandas DataFrame containing the discharge time series for each river, indexed by timestamp.
 
         Raises:
             ValueError: If there is not enough data available for reliable spinup.
         """
-        da: xr.DataArray = read_zarr(
-            self.model.output_folder
-            / "report"
-            / "spinup"
-            / "hydrology.routing"
-            / "discharge_hourly.zarr"
-        )
+        rivers: gpd.GeoDataFrame = self.model.hydrology.routing.get_active_rivers()
+        discharge = pd.DataFrame()
+        for river_id, river in rivers.iterrows():
+            discharge[river_id] = read_table(
+                self.model.reporter.report_folder.parent
+                / "spinup"
+                / "hydrology.routing"
+                / f"river_outflow_hourly_m3_per_s_{river_id}.parquet"
+            )[f"river_outflow_hourly_m3_per_s_{river_id}"]
 
-        start_time = pd.to_datetime(da.time[0].item()) + pd.DateOffset(years=10)
-        da: xr.DataArray = da.sel(time=slice(start_time, da.time[-1]))
+        start_time = discharge.index[0] + pd.DateOffset(years=10)
+        discharge = discharge.loc[start_time:]
 
         # make sure there is at least 20 years of data
-        if len(da.time) == 0 or len(da.time.groupby(da.time.dt.year).groups) < 20:
+        if (discharge.index[-1].year - discharge.index[0].year) < 20:
             raise ValueError(
                 """Not enough data available for reliable spinup, should be at least 20 years of data left.
                 Please run the model for at least 30 years (10 years of data is discarded)."""
             )
 
-        return da
+        return discharge
 
     @property
     def mannings(self) -> xr.DataArray:
