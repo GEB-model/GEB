717511e6cf38ddd5453c156e916ec3e8502b8e84
Jens de Bruijn <2176353+jensdebruijn@users.noreply.github.com>
2026-04-23 08:39:03 +0200

    revert accidental change

--- geb/agents/insurers.py
+++ geb/agents/insurers.py
@@ -877,8 +877,6 @@ class Insurers(AgentBaseClass):
             )
             return pr_premium, farmer_yield_probability_relation_insured_pr
 
-        return None
-
         msg = "No insurance adaptation is active."
         raise ValueError(msg)
 
