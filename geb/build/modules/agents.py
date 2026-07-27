7c14dfeedf7e35cd9c3e37287994bd3d039d2b84
Tierolf <lars_tierolf@hotmail.com>
2026-07-27 13:53:51 +0200

    resolve remaining conflicts

--- geb/build/modules/agents.py
+++ geb/build/modules/agents.py
@@ -1538,7 +1538,6 @@ class Agents(BuildModelBase):
         Returns:
             Buildings with distance columns appended (meters).
         """
-
         rivers: gpd.GeoDataFrame = gpd.read_parquet(
             "input/" + self.files["geom"]["routing/rivers"]
         )[["geometry"]]
@@ -1607,7 +1606,7 @@ class Agents(BuildModelBase):
         buildings = buildings.to_crs(buildings_crs)
 
         return buildings
-=======
+
     def assign_subbasins_to_buildings(
         self, buildings: gpd.GeoDataFrame
     ) -> gpd.GeoDataFrame:
@@ -1629,7 +1628,6 @@ class Agents(BuildModelBase):
             buildings_with_subbasin["COMID"].fillna(-1).astype(int)
         )
         return buildings_with_subbasin
->>>>>>> 41c77efa (Lars dynamic fps (#905))
 
     @build_method(required=True)
     def setup_buildings(self) -> None:
@@ -1644,7 +1642,6 @@ class Agents(BuildModelBase):
         buildings = self.calculate_distance_parameters(buildings)
 =======
         buildings = self.assign_subbasins_to_buildings(buildings)
->>>>>>> 41c77efa (Lars dynamic fps (#905))
 
         # reset id column to avoid issues with duplicate ids
         buildings["id"] = np.arange(len(buildings))
