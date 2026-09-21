60da4099485db3cb065a0b6dccae323321212ba3
Tierolf <lars_tierolf@hotmail.com>
2026-09-21 10:00:48 +0200

    do not overwrite multirun results

--- geb/cli.py
+++ geb/cli.py
@@ -345,10 +345,13 @@ def run_yearly(multi: bool, n_runs: int | None, prefix: str, **kwargs: Any) -> N
     for run_id in range(n_runs):
         wd = kwargs["working_directory"]
         run_name = f"{prefix}run_{run_id}"
-        output_path = wd / "output" / run_name / "report" / "agents.households"
-        if output_path.exists():
+        output_path_agents = wd / "output" / run_name / "report" / "agents.households"
+        output_path_government = (
+            wd / "output" / run_name / "report" / "agents.government"
+        )
+        if output_path_agents.exists() and output_path_government.exists():
             click.echo(
-                f"Skipping run {run_id} as output already exists at {output_path}.",
+                f"Skipping run {run_id} as output already exists at {output_path_agents}.",
                 err=True,
             )
         else:
